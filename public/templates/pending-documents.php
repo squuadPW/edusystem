dd59e6aef25fbffca76e7b6d6011f7c883deaa4c
Junnior Rivas <jrdrivasg@gmail.com>
2024-07-10 17:40:24 -0400

    fix

--- public/templates/pending-documents.php
+++ public/templates/pending-documents.php
@@ -5,27 +5,66 @@
         <?php if(!empty($students)): ?>
            <?php foreach($students as $student): ?>
                 <?php $documents = get_documents($student->id); ?>
+                <div style="border: 1px solid #091c5c; background-color: #f2f2f3; padding: 18px; border-radius: 15px;">
                 <?php foreach($documents as $document): ?>
                     <?php if($document->status == 0 || $document->status == 1 || $document->status == 3 || $document->status == 4): ?>
-                    <?php if($document->status == 0 || $document->status == 1): ?>
-                        <div class="woocommerce-info" role="alert" style="margin-bottom:10px;">
-                    <?php elseif($document->status == 3): ?>
-                        <div class="woocommerce-error" role="alert" style="margin-bottom:10px;">
-                    <?php elseif($document->status == 4): ?>
-                        <div class="woocommerce-error" role="alert" style="margin-bottom:10px;">
-                    <?php endif; ?>
-                            <div style="display:flex;width:100%;flex-direction:row;">
-                                <span style="width:70%">
-                                    <?php $name = get_name_document($document->document_id); ?>
-                                    <?= $student->name.' '.$student->last_name.' - '.$name; ?>
-                                </span>
-                                <span style="width:30%;text-align:end;">
-                                    <?= $status = get_status_document($document->status); ?>
-                                </span>
-                            </div>
+                        <div style="display:flex;width:100%;flex-direction:row; margin-bottom: 10px">
+                            <span style="width:70%">
+                                <?php 
+                                    $status = get_status_document($document->status);
+                                    if($status === 'No sent') { ?>
+                                        <span>
+                                            <?php $name = get_name_document($document->document_id); ?>
+                                            <?= $student->name.' '.$student->last_name.' - '.$name; ?>
+                                        </span>
+                                    <?php } elseif($status === 'Sent') { ?>
+                                        <span style="color: blue">
+                                            <?php $name = get_name_document($document->document_id); ?>
+                                            <?= $student->name.' '.$student->last_name.' - '.$name; ?>
+                                        </span>
+                                    <?php } elseif($status === 'Processing') { ?>
+                                        <span style="color: yellow">
+                                            <?php $name = get_name_document($document->document_id); ?>
+                                            <?= $student->name.' '.$student->last_name.' - '.$name; ?>
+                                        </span>
+                                    <?php } elseif($status === 'Declined' || $status === 'Expired') { ?>
+                                        <span style="color: red">
+                                            <?php $name = get_name_document($document->document_id); ?>
+                                            <?= $student->name.' '.$student->last_name.' - '.$name; ?>
+                                        </span>
+                                    <?php } elseif($status === 'Approved') { ?>
+                                        <span style="color: green">
+                                            <?php $name = get_name_document($document->document_id); ?>
+                                            <?= $student->name.' '.$student->last_name.' - '.$name; ?>
+                                        </span>
+                                    <?php } else { ?>
+                                        <span>
+                                            <?php $name = get_name_document($document->document_id); ?>
+                                            <?= $student->name.' '.$student->last_name.' - '.$name; ?>
+                                        </span>
+                                    <?php } ?>
+                            </span>
+                            <span style="width:30%;text-align:end;">
+                                <?php 
+                                $status = get_status_document($document->status);
+                                if($status === 'No sent') { ?>
+                                    <span><?= $status ?></span>
+                                <?php } elseif($status === 'Sent') { ?>
+                                    <span style="color: blue"><?= $status ?></span>
+                                <?php } elseif($status === 'Processing') { ?>
+                                    <span style="color: yellow"><?= $status ?></span>
+                                <?php } elseif($status === 'Declined' || $status === 'Expired') { ?>
+                                    <span style="color: red"><?= $status ?></span>
+                                <?php } elseif($status === 'Approved') { ?>
+                                    <span style="color: green"><?= $status ?></span>
+                                <?php } else { ?>
+                                    <span><?= $status ?></span>
+                                <?php } ?>
+                            </span>
                         </div>
                     <?php endif; ?>
                 <?php endforeach; ?>
+                </div>
             <?php endforeach; ?>
         <?php endif; ?>
        
