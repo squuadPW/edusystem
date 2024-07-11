2d055346f17345aeffc709282cbc3c45d8cddf49
Junnior Rivas <jrdrivasg@gmail.com>
2024-07-11 15:46:06 -0400

    fix

--- admin/user.php
+++ admin/user.php
@@ -5,6 +5,7 @@ function delete_data_student($user_id){
     global $wpdb;
     $table_student_documents = $wpdb->prefix.'student_documents';
     $table_students = $wpdb->prefix.'students';
+    $table_student_payments = $wpdb->prefix.'student_payment';
 
     $students = $wpdb->get_results("SELECT * FROM {$table_students} WHERE partner_id={$user_id}");
 
@@ -17,6 +18,14 @@ function delete_data_student($user_id){
             foreach($documents as $document){
 
                 $wpdb->delete($table_student_documents,['id' => $document->id]);
+            }
+
+            
+            $payments = $wpdb->get_results("SELECT * FROM {$table_student_payments} WHERE student_id={$student->id}");
+
+            foreach($payments as $document){
+
+                $wpdb->delete($table_student_payments,['id' => $document->id]);
             }
 
             $wpdb->delete($table_students,['id' => $student->id]);
