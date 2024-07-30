34dfb25a51336e4c9d082e627e5f5746bb5c096a
Jose Mora <jmoraw7@gmail.com>
2024-07-30 10:19:41 -0400

    fix

--- admin/laravelRequests.php
+++ admin/laravelRequests.php
@@ -1,9 +1,10 @@
 <?php
 
 function create_user_laravel($fields, $files) {
-  $url = 'https://ppadmin.american-elite.us/api/public/create-student';
+  // $url = 'https://ppadmin.american-elite.us/';
+  $url = 'https://5128-190-142-88-208.ngrok-free.app/';
 
-  $ch = curl_init($url);
+  $ch = curl_init($url.'api/public/create-student');
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($ch, CURLOPT_POST, true);
 
