ef791d3a89154ca797b17119c297597f6ddfd6c0
Junnior Rivas <jrdrivasg@gmail.com>
2024-08-06 17:18:34 -0400

    minor fix

--- settings.php
+++ settings.php
@@ -3,13 +3,10 @@
     // AWS
 	// define( 'AES_FEE_INSCRIPTION','63' );
 	// define( 'AES_LATAM_SCHOOLARSHIP','62' );
-
 	// define( 'AES_DUAL_9NO','51'); // Product Initial 9 Grade
 	// define( 'AES_DUAL_9NO_VARIABLE','54'); // Product variable
-
 	// define( 'AES_DUAL_10MO', '52' );// Product Initial 10mo Grade
 	// define( 'AES_DUAL_10MO_VARIABLE','57'); // Product variable
-
 	// define( 'AES_DUAL_DEFAULT', '53' );    // Product initial 11 and bacheloder
 	// define( 'AES_DUAL_DEFAULT_VARIABLE','60'); // Product variable
 	// define( 'ROLES_OF_STAFF', array('administrator', 'administrador', 'admision', 'admission', 'alliance', 'finanzas', 'allied'));
@@ -17,13 +14,10 @@
 	// DREAM HOST
 	// define( 'AES_FEE_INSCRIPTION','527' );
 	// define( 'AES_LATAM_SCHOOLARSHIP','454' );
-
 	// define( 'AES_DUAL_9NO','516'); // Product Initial 9 Grade
 	// define( 'AES_DUAL_9NO_VARIABLE','519'); // Product variable
-
 	// define( 'AES_DUAL_10MO', '517' );// Product Initial 10mo Grade
 	// define( 'AES_DUAL_10MO_VARIABLE','522'); // Product variable
-
 	// define( 'AES_DUAL_DEFAULT', '518' );    // Product initial 11 and bacheloder
 	// define( 'AES_DUAL_DEFAULT_VARIABLE','525'); // Product variable
 	// define( 'ROLES_OF_STAFF', array('administrator', 'administration', 'admission', 'alliance', 'finance', 'admision', 'finanzas'));
@@ -31,13 +25,10 @@
 	// LOCAL JOSE MORA
 	define( 'AES_FEE_INSCRIPTION','484' );
 	define( 'AES_LATAM_SCHOOLARSHIP','481' );
-
 	define( 'AES_DUAL_9NO','465'); // Product Initial 9 Grade
 	define( 'AES_DUAL_9NO_VARIABLE','468'); // Product variable
-
 	define( 'AES_DUAL_10MO', '466' );// Product Initial 10mo Grade
 	define( 'AES_DUAL_10MO_VARIABLE','472'); // Product variable
-
 	define( 'AES_DUAL_DEFAULT', '467' );    // Product initial 11 and bacheloder
 	define( 'AES_DUAL_DEFAULT_VARIABLE','475'); // Product variable
 	define( 'ROLES_OF_STAFF', array('administrator', 'administration', 'admission', 'alliance', 'finance', 'admision', 'finanzas'));
