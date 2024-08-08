83dd663c697f279446a0b3d78672b26f5339c0b8
Junnior Rivas <jrdrivasg@gmail.com>
2024-08-08 10:36:30 -0400

    fixes

--- settings.php
+++ settings.php
@@ -9,7 +9,7 @@
 	// define( 'AES_DUAL_10MO_VARIABLE','57'); // Product variable
 	// define( 'AES_DUAL_DEFAULT', '53' );    // Product initial 11 and bacheloder
 	// define( 'AES_DUAL_DEFAULT_VARIABLE','60'); // Product variable
-	// define( 'ROLES_OF_STAFF', array('owner', 'administrator', 'administrador', 'admision', 'admission', 'alliance', 'administration', 'allied'));
+	// define( 'ROLES_OF_STAFF', array('owner', 'administrador', 'admision', 'admission', 'alliance', 'administration', 'allied'));
      
 	// DREAM HOST
 	// define( 'AES_FEE_INSCRIPTION','527' );
