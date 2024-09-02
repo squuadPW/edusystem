f5b6cb25a7168b15d632c4528edb35a1351ec589
Junnior Rivas <jrdrivasg@gmail.com>
2024-09-02 10:29:19 -0400

    fixes

--- admin/payments.php
+++ admin/payments.php
@@ -1,10 +1,11 @@
 <?php
 
-function add_admin_form_payments_content(){
+function add_admin_form_payments_content()
+{
 
-    if(isset($_GET['action']) && !empty($_GET['action'])){
+    if (isset($_GET['action']) && !empty($_GET['action'])) {
 
-        if($_GET['action'] == 'change_status_payment'){
+        if ($_GET['action'] == 'change_status_payment') {
 
             $order_id = $_POST['order_id'];
 
@@ -17,110 +18,122 @@ function add_admin_form_payments_content(){
         }
     }
 
-    if(isset($_GET['section_tab']) && !empty($_GET['section_tab'])){
+    if (isset($_GET['section_tab']) && !empty($_GET['section_tab'])) {
 
 
-        if($_GET['section_tab'] == 'all_payments'){
+        if ($_GET['section_tab'] == 'all_payments') {
             $list_payments = new TT_all_payments_List_Table;
             $list_payments->prepare_items();
-            include(plugin_dir_path(__FILE__).'templates/list-payments.php');
-        }else if($_GET['section_tab'] == 'order_detail'){
+            include(plugin_dir_path(__FILE__) . 'templates/list-payments.php');
+        } else if ($_GET['section_tab'] == 'order_detail') {
 
             global $current_user;
             $roles = $current_user->roles;
             $order_id = $_GET['order_id'];
             $order = wc_get_order($order_id);
 
-            include(plugin_dir_path(__FILE__).'templates/payment-details.php');
+            include(plugin_dir_path(__FILE__) . 'templates/payment-details.php');
         }
 
-    }else{
+    } else {
         $list_payments = new TT_payment_pending_List_Table;
         $list_payments->prepare_items();
-        include(plugin_dir_path(__FILE__).'templates/list-payments.php');
+        include(plugin_dir_path(__FILE__) . 'templates/list-payments.php');
     }
 }
 
 
-class TT_payment_pending_List_Table extends WP_List_Table{
+class TT_payment_pending_List_Table extends WP_List_Table
+{
 
-	function __construct(){
-        global $status, $page,$categories;
-         
-        parent::__construct( array(
-            'singular'  => 'payment_pending',    
-            'plural'    => 'payment_pendings',
-            'ajax'      => true
-        ) );
-        
+    function __construct()
+    {
+        global $status, $page, $categories;
+
+        parent::__construct(array(
+            'singular' => 'payment_pending',
+            'plural' => 'payment_pendings',
+            'ajax' => true
+        ));
+
     }
 
-	function column_default($item, $column_name){
+    function column_default($item, $column_name)
+    {
 
         global $current_user;
 
-        switch($column_name){
+        switch ($column_name) {
             case 'payment_id':
-                return '#'.$item[$column_name];
+                return '#' . $item[$column_name];
             case 'date':
             case 'status':
             case 'payment_method':
             case 'partner_name':
                 return ucwords($item[$column_name]);
             case 'total':
-                return '<b>'.$item[$column_name].'</b>';
+                return '<b>' . $item[$column_name] . '</b>';
             case 'view_details':
-                return "<a href='".admin_url('/admin.php?page=add_admin_form_payments_content&section_tab=order_detail&order_id='.$item['payment_id'])."' class='button button-primary'>".__('View Details','form-plugin')."</a>";
-			default:
-				return print_r($item,true);
+                return "<a href='" . admin_url('/admin.php?page=add_admin_form_payments_content&section_tab=order_detail&order_id=' . $item['payment_id']) . "' class='button button-primary'>" . __('View Details', 'form-plugin') . "</a>";
+            default:
+                return print_r($item, true);
         }
     }
 
-	function column_name($item){
+    function column_name($item)
+    {
 
-        return sprintf('%1$s<a href="javascript:void(0)">%2$s</a>',
-            '<span data-id="'.$item['id'].'" class="dashicons dashicons-menu handle" style="cursor:all-scroll;"></span>',
+        return sprintf(
+            '%1$s<a href="javascript:void(0)">%2$s</a>',
+            '<span data-id="' . $item['id'] . '" class="dashicons dashicons-menu handle" style="cursor:all-scroll;"></span>',
             ucwords($item['name']),
         );
     }
 
-	function column_cb($item){
+    function column_cb($item)
+    {
         return '';
     }
 
-	function get_columns(){
+    function get_columns()
+    {
 
         $columns = array(
-            'payment_id'     => __('Payment ID','aes'),
-            'date'     => __('Date','aes'),
-            'partner_name'  => __('Name','aes'),
-            'total' => __('Total','aes'),
-            'payment_method' => __('Payment Method','aes'),
-            'status' => __('Status','aes'),
-            'view_details' => __('Actions','aes'),
+            'payment_id' => __('Payment ID', 'aes'),
+            'date' => __('Date', 'aes'),
+            'partner_name' => __('Name', 'aes'),
+            'total' => __('Total', 'aes'),
+            'payment_method' => __('Payment Method', 'aes'),
+            'status' => __('Status', 'aes'),
+            'view_details' => __('Actions', 'aes'),
         );
 
         return $columns;
     }
 
-    function get_payment_pendings(){
+    function get_payment_pendings()
+    {
         $orders_array = [];
         $args = [];
+        $per_page = 20; // number of items per page
+        $pagenum = isset($_GET['paged']) ? absint($_GET['paged']) : 1;
+        $offset = (($pagenum - 1) * $per_page);
 
-        if(isset($_POST['s']) && !empty($_POST['s'])){
+        if (isset($_POST['s']) && !empty($_POST['s'])) {
             $args['s'] = $_POST['s'];
         }
 
-        $args['limit'] = -1;
-        $args['status'] = array('wc-pending','wc-cancelled','wc-processing','wc-on-hold');
+        $args['limit'] = $per_page; // limit to 10 orders per page
+        $args['offset'] = $offset; // offset to start from the first order
+        $args['status'] = array('wc-pending', 'wc-cancelled', 'wc-processing', 'wc-on-hold');
         $orders = wc_get_orders($args);
 
-        if($orders){
-            foreach($orders as $order){
-                array_push($orders_array,[
+        if ($orders) {
+            foreach ($orders as $order) {
+                array_push($orders_array, [
                     'payment_id' => $order->get_id(),
                     'date' => $order->get_date_created()->format('F j, Y g:i a'),
-                    'partner_name' =>  $order->get_billing_first_name().' '.$order->get_billing_last_name(),
+                    'partner_name' => $order->get_billing_first_name() . ' ' . $order->get_billing_last_name(),
                     'total' => wc_price($order->get_total()),
                     'status' => $order->get_status(),
                     'payment_method' => $order->get_payment_method_title()
@@ -128,138 +141,155 @@ class TT_payment_pending_List_Table extends WP_List_Table{
             }
         }
 
-        return $orders_array;
-    }   
+        $args_filtered['limit'] = -1;
+        $args_filtered['status'] = array('wc-pending', 'wc-cancelled', 'wc-processing', 'wc-on-hold');
+        $total_count = wc_get_orders(array_merge($args_filtered, array('return' => 'count')));
+        return ['data' => $orders_array, 'total_count' => sizeof($total_count)];
+    }
 
-	function get_sortable_columns() {
+    function get_sortable_columns()
+    {
         $sortable_columns = [];
         return $sortable_columns;
     }
 
-	
-	function get_bulk_actions() {
+
+    function get_bulk_actions()
+    {
         $actions = [];
         return $actions;
     }
 
-	function process_bulk_action(){
-        
+    function process_bulk_action()
+    {
+
         //Detect when a bulk action is being triggered...
-        if( 'delete'===$this->current_action() ) {
+        if ('delete' === $this->current_action()) {
             wp_die('Items deleted (or they would be if we had items to delete)!');
-        }  
+        }
     }
 
-	function prepare_items(){
+    function prepare_items()
+    {
 
-		$data_payments = $this->get_payment_pendings();
-
-		$per_page = 10;
-
-		  
+        $data_payments = $this->get_payment_pendings();
         $columns = $this->get_columns();
         $hidden = array();
         $sortable = $this->get_sortable_columns();
-
+        $data = $data_payments['data'];
+        $total_count = (int) $data_payments['total_count'];
         $this->_column_headers = array($columns, $hidden, $sortable);
-		$this->process_bulk_action();
-        $data = $data_payments;
+        $this->process_bulk_action();
 
-		function usort_reorder($a,$b){
+        function usort_reorder($a, $b)
+        {
             $orderby = (!empty($_REQUEST['orderby'])) ? $_REQUEST['orderby'] : 'order';
-            $order = (!empty($_REQUEST['order'])) ? $_REQUEST['order'] : 'asc'; 
-            $result = strcmp($a[$orderby], $b[$orderby]); 
-            return ($order==='asc') ? $result : -$result;
+            $order = (!empty($_REQUEST['order'])) ? $_REQUEST['order'] : 'asc';
+            $result = strcmp($a[$orderby], $b[$orderby]);
+            return ($order === 'asc') ? $result : -$result;
         }
-       
-		$current_page = $this->get_pagenum();
-    
-        $total_items = count($data);
-		
+
+        $per_page = 20; // items per page
+        $this->set_pagination_args(array(
+            'total_items' => $total_count,
+            'per_page' => $per_page,
+        ));
         $this->items = $data;
-	}
+    }
 
 }
 
-class TT_all_payments_List_Table extends WP_List_Table{
+class TT_all_payments_List_Table extends WP_List_Table
+{
 
-	function __construct(){
-        global $status, $page,$categories;
-         
-        parent::__construct( array(
-            'singular'  => 'payment_pending',    
-            'plural'    => 'payment_pendings',
-            'ajax'      => true
-        ) );
-        
+    function __construct()
+    {
+        global $status, $page, $categories;
+
+        parent::__construct(array(
+            'singular' => 'payment_pending',
+            'plural' => 'payment_pendings',
+            'ajax' => true
+        ));
+
     }
 
-	function column_default($item, $column_name){
+    function column_default($item, $column_name)
+    {
 
         global $current_user;
 
-        switch($column_name){
+        switch ($column_name) {
             case 'payment_id':
-                return '#'.$item[$column_name];
+                return '#' . $item[$column_name];
             case 'date':
             case 'payment_method':
             case 'status':
             case 'partner_name':
                 return ucwords($item[$column_name]);
             case 'total':
-                return '<b>'.$item[$column_name].'</b>';
+                return '<b>' . $item[$column_name] . '</b>';
             case 'view_details':
-                return "<a href='".admin_url('/admin.php?page=add_admin_form_payments_content&section_tab=order_detail&order_id='.$item['payment_id'])."' class='button button-primary'>".__('View Details','form-plugin')."</a>";
-			default:
-				return print_r($item,true);
+                return "<a href='" . admin_url('/admin.php?page=add_admin_form_payments_content&section_tab=order_detail&order_id=' . $item['payment_id']) . "' class='button button-primary'>" . __('View Details', 'form-plugin') . "</a>";
+            default:
+                return print_r($item, true);
         }
     }
 
-	function column_name($item){
+    function column_name($item)
+    {
 
-        return sprintf('%1$s<a href="javascript:void(0)">%2$s</a>',
-            '<span data-id="'.$item['id'].'" class="dashicons dashicons-menu handle" style="cursor:all-scroll;"></span>',
+        return sprintf(
+            '%1$s<a href="javascript:void(0)">%2$s</a>',
+            '<span data-id="' . $item['id'] . '" class="dashicons dashicons-menu handle" style="cursor:all-scroll;"></span>',
             ucwords($item['name']),
         );
     }
 
-	function column_cb($item){
+    function column_cb($item)
+    {
         return '';
     }
 
-	function get_columns(){
+    function get_columns()
+    {
 
         $columns = array(
-            'payment_id'     => __('Payment ID','aes'),
-            'date'     => __('Date','aes'),
-            'partner_name'  => __('Name','aes'),
-            'total' => __('Total','aes'),
-            'payment_method' => __('Payment Method','aes'),
-            'status' => __('Status','aes'),
-            'view_details' => __('Actions','aes'),
+            'payment_id' => __('Payment ID', 'aes'),
+            'date' => __('Date', 'aes'),
+            'partner_name' => __('Name', 'aes'),
+            'total' => __('Total', 'aes'),
+            'payment_method' => __('Payment Method', 'aes'),
+            'status' => __('Status', 'aes'),
+            'view_details' => __('Actions', 'aes'),
         );
 
         return $columns;
     }
 
-    function get_payment(){
+    function get_payment()
+    {
         $orders_array = [];
         $args = [];
+        $per_page = 20; // number of items per page
+        $pagenum = isset($_GET['paged']) ? absint($_GET['paged']) : 1;
+        $offset = (($pagenum - 1) * $per_page);
 
-        if(isset($_POST['s']) && !empty($_POST['s'])){
+        if (isset($_POST['s']) && !empty($_POST['s'])) {
             $args['s'] = $_POST['s'];
         }
 
-        $args['limit'] = -1;
-        $args['status'] = array('wc-pending','wc-completed','wc-cancelled','wc-processing','wc-on-hold');
+        $args['limit'] = $per_page; // limit to 10 orders per page
+        $args['offset'] = $offset; // offset to start from the first order
+        $args['status'] = array('wc-pending', 'wc-completed', 'wc-cancelled', 'wc-processing', 'wc-on-hold');
         $orders = wc_get_orders($args);
 
-        if($orders){
-            foreach($orders as $order){
-                array_push($orders_array,[
+        if ($orders) {
+            foreach ($orders as $order) {
+                array_push($orders_array, [
                     'payment_id' => $order->get_id(),
                     'date' => $order->get_date_created()->format('F j, Y g:i a'),
-                    'partner_name' =>  $order->get_billing_first_name().' '.$order->get_billing_last_name(),
+                    'partner_name' => $order->get_billing_first_name() . ' ' . $order->get_billing_last_name(),
                     'total' => wc_price($order->get_total()),
                     'status' => $order->get_status(),
                     'payment_method' => $order->get_payment_method_title()
@@ -267,55 +297,62 @@ class TT_all_payments_List_Table extends WP_List_Table{
             }
         }
 
-        return $orders_array;
-    }   
+        $args_filtered['limit'] = -1;
+        $args_filtered['status'] = array('wc-pending', 'wc-completed', 'wc-cancelled', 'wc-processing', 'wc-on-hold');
+        $total_count = wc_get_orders(array_merge($args_filtered, array('return' => 'count')));
+        return ['data' => $orders_array, 'total_count' => sizeof($total_count)];
+    }
 
-	function get_sortable_columns() {
+    function get_sortable_columns()
+    {
         $sortable_columns = [];
         return $sortable_columns;
     }
 
-	
-	function get_bulk_actions() {
+
+    function get_bulk_actions()
+    {
         $actions = [];
         return $actions;
     }
 
-	function process_bulk_action(){
-        
+    function process_bulk_action()
+    {
+
         //Detect when a bulk action is being triggered...
-        if( 'delete'===$this->current_action() ) {
+        if ('delete' === $this->current_action()) {
             wp_die('Items deleted (or they would be if we had items to delete)!');
-        }  
+        }
     }
 
-	function prepare_items(){
+    function prepare_items()
+    {
 
-		$data_payments = $this->get_payment();
-
-		$per_page = 10;
-
-		  
+        $data_payments = $this->get_payment();
         $columns = $this->get_columns();
         $hidden = array();
         $sortable = $this->get_sortable_columns();
 
         $this->_column_headers = array($columns, $hidden, $sortable);
-		$this->process_bulk_action();
-        $data = $data_payments;
+        $this->process_bulk_action();
+        $data = $data_payments['data'];
+        $total_count = (int) $data_payments['total_count'];
 
-		function usort_reorder($a,$b){
+        function usort_reorder($a, $b)
+        {
             $orderby = (!empty($_REQUEST['orderby'])) ? $_REQUEST['orderby'] : 'order';
-            $order = (!empty($_REQUEST['order'])) ? $_REQUEST['order'] : 'asc'; 
-            $result = strcmp($a[$orderby], $b[$orderby]); 
-            return ($order==='asc') ? $result : -$result;
+            $order = (!empty($_REQUEST['order'])) ? $_REQUEST['order'] : 'asc';
+            $result = strcmp($a[$orderby], $b[$orderby]);
+            return ($order === 'asc') ? $result : -$result;
         }
-       
-		$current_page = $this->get_pagenum();
-    
-        $total_items = count($data);
-		
+
+        $per_page = 20; // items per page
+        $this->set_pagination_args(array(
+            'total_items' => $total_count,
+            'per_page' => $per_page,
+        ));
+
         $this->items = $data;
-	}
+    }
 
 }
