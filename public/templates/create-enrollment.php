e317046a0165a800146262f265d5e53bdcc9261d
Junnior Rivas <jrdrivasg@gmail.com>
2024-09-06 16:03:22 -0400

    fix

--- public/templates/create-enrollment.php
+++ public/templates/create-enrollment.php
@@ -12,45 +12,45 @@
 
 <script src="https://cdn.jsdelivr.net/npm/signature_pad@4.1.7/dist/signature_pad.umd.min.js"></script>
 <script>
-const signaturePad = new SignaturePad(document.getElementById('signature-pad'));
-document.getElementById('clear').addEventListener('click', () => {
-  signaturePad.clear();
-});
+    const signaturePad = new SignaturePad(document.getElementById('signature-pad'));
+    document.getElementById('clear').addEventListener('click', () => {
+        signaturePad.clear();
+    });
 
-function resizeCanvas() {
-    const canvas = document.getElementById('signature-pad');
-    const ratio =  Math.max(window.devicePixelRatio || 1, 1);
-    let width, height;
+    function resizeCanvas() {
+        const canvas = document.getElementById('signature-pad');
+        const ratio = Math.max(window.devicePixelRatio || 1, 1);
+        let width, height;
 
-    // Set different canvas sizes based on screen sizes
-    if (window.matchMedia("(max-width: 768px)").matches) { // mobile
-        width = 300;
-        height = 150;
-    } else if (window.matchMedia("(max-width: 1200px)").matches) { // laptop
-        width = 400;
-        height = 200;
-    } else { // desktop
-        width = 600;
-        height = 300;
+        // Set different canvas sizes based on screen sizes
+        if (window.matchMedia("(max-width: 768px)").matches) { // mobile
+            width = 300;
+            height = 150;
+        } else if (window.matchMedia("(max-width: 1200px)").matches) { // laptop
+            width = 400;
+            height = 200;
+        } else { // desktop
+            width = 600;
+            height = 300;
+        }
+
+        canvas.width = width * ratio;
+        canvas.height = height * ratio;
+        canvas.style.width = width + 'px';
+        canvas.style.height = height + 'px';
+        canvas.getContext("2d").scale(ratio, ratio);
+        signaturePad.clear(); // otherwise isEmpty() might return incorrect value
     }
 
-    canvas.width = width * ratio;
-    canvas.height = height * ratio;
-    canvas.style.width = width + 'px';
-    canvas.style.height = height + 'px';
-    canvas.getContext("2d").scale(ratio, ratio);
-    signaturePad.clear(); // otherwise isEmpty() might return incorrect value
-}
+    window.addEventListener("resize", resizeCanvas);
+    window.addEventListener("orientationchange", resizeCanvas);
 
-window.addEventListener("resize", resizeCanvas);
-window.addEventListener("orientationchange", resizeCanvas);
+    document.addEventListener("DOMContentLoaded", function () {
+        const metaViewport = document.querySelector('meta[name="viewport"]');
+        if (metaViewport) {
+            metaViewport.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no";
+        }
+    });
 
-document.addEventListener("DOMContentLoaded", function() {
-  const metaViewport = document.querySelector('meta[name="viewport"]');
-  if (metaViewport) {
-    metaViewport.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no";
-  }
-});
-
-resizeCanvas();
+    resizeCanvas();
 </script>
