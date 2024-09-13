4fcac0c9790ab40c8b492fe51872623718d837f2
Junnior Rivas <jrdrivasg@gmail.com>
2024-09-13 16:07:41 -0400

    "Modified checkout.js to add click event listener and conditional beforeunload event prevention"

--- public/assets/js/checkout.js
+++ public/assets/js/checkout.js
@@ -5,8 +5,20 @@ document.addEventListener('DOMContentLoaded',function(){
         document.getElementById('createaccount').checked = true;
     }
 
-    window.addEventListener('beforeunload', function(event) {
-        event.preventDefault();
+    place_order = false;
+    document.addEventListener('click', function(event) {
+        if (event.target.id === 'place_order') {
+            place_order = true;
+        }
     });
+
+    const checkoutRoute = '/checkout/'; // adjust this to your actual checkout route
+    if (window.location.pathname === checkoutRoute) {
+      window.addEventListener('beforeunload', function(event) {
+        if (!place_order) {
+            event.preventDefault();
+        }
+      });
+    }   
 
 });
