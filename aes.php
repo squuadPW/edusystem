aca079a4cb58d27469ede3347c963cb1226c51aa
Junnior Rivas <jrdrivasg@gmail.com>
2024-09-16 16:42:00 -0400

    fix

--- aes.php
+++ aes.php
@@ -44,7 +44,7 @@ function create_tables() {
         id INT(11) NOT NULL AUTO_INCREMENT,
         user_id INT(11) NOT NULL,
         message LONGTEXT NOT NULL,
-        read BOOLEAN DEFAULT 0,
+        `read` BOOLEAN NOT NULL DEFAULT 0,
         type_notice TEXT NOT NULL,
         importance INT(11) NOT NULL,
         created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
