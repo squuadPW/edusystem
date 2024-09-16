a07c7af856bc69693a41242ffed9f65da922f1e5
Junnior Rivas <jrdrivasg@gmail.com>
2024-09-16 13:29:42 -0400

    "Added new sections for invoices of alliances and institutes to payments admin page, including list tables and functionality to update payment status."

--- admin/payments.php
+++ admin/payments.php
@@ -38,6 +38,23 @@ function add_admin_form_payments_content()
             $order = wc_get_order($order_id);
 
             include(plugin_dir_path(__FILE__) . 'templates/payment-details.php');
+        } else if ($_GET['section_tab'] == 'invoices_alliances') {
+
+            if ($_GET['id_payment']) {
+                global $wpdb;
+                $table_alliances_payments = $wpdb->prefix . 'alliances_payments';
+                $wpdb->update($table_alliances_payments, ['status_id' => 1], ['id' => $_GET['id_payment']]);
+            }
+
+            $list_payments = new TT_Invoices_Alliances_List_Table;
+            $list_payments->prepare_items();
+            include(plugin_dir_path(__FILE__) . 'templates/list-invoices-alliance.php');
+        } else if ($_GET['section_tab'] == 'invoices_institutes') {
+
+            $list_payments = new TT_all_payments_List_Table;
+            $list_payments->prepare_items();
+
+            include(plugin_dir_path(__FILE__) . 'templates/list-invoices-institutes.php');
         }
 
     } else {
@@ -342,6 +359,146 @@ class TT_all_payments_List_Table extends WP_List_Table
         $this->process_bulk_action();
         $data = $data_payments['data'];
         $total_count = (int) $data_payments['total_count'];
+
+        function usort_reorder($a, $b)
+        {
+            $orderby = (!empty($_REQUEST['orderby'])) ? $_REQUEST['orderby'] : 'order';
+            $order = (!empty($_REQUEST['order'])) ? $_REQUEST['order'] : 'asc';
+            $result = strcmp($a[$orderby], $b[$orderby]);
+            return ($order === 'asc') ? $result : -$result;
+        }
+
+        $per_page = 20; // items per page
+        $this->set_pagination_args(array(
+            'total_items' => $total_count,
+            'per_page' => $per_page,
+        ));
+
+        $this->items = $data;
+    }
+
+}
+
+class TT_Invoices_Alliances_List_Table extends WP_List_Table
+{
+
+    function __construct()
+    {
+        global $status, $page, $categories;
+
+        parent::__construct(array(
+            'singular' => 'invoice_alliance',
+            'plural' => 'invoices_alliances',
+            'ajax' => true
+        ));
+
+    }
+
+    function column_default($item, $column_name)
+    {
+
+        global $current_user;
+
+        switch ($column_name) {
+            case 'status_id':
+                return get_status_payment_institute($item->status_id);
+            case 'alliance_id':
+                $alliance = get_alliance_detail($item->alliance_id);
+                return "{$alliance->name} {$alliance->last_name} - {$alliance->name_legal}";
+            case 'month':
+                return $item->month;
+            case 'amount':
+                return wc_price($item->amount);
+            case 'total_orders':
+                return $item->total_orders;
+            case 'view_details':
+                if ($item->status_id == 0) {
+                    return "<a href='" . admin_url('/admin.php?page=add_admin_form_payments_content&section_tab=invoices_alliances&id_payment=' . $item->id) . "' class='button button-primary'>" . __('Pay', 'form-plugin') . "</a>";
+                } else {
+                    return "N/A";
+                }
+            default:
+                return print_r($item, true);
+        }
+    }
+
+    function column_name($item)
+    {
+
+        return sprintf(
+            '%1$s<a href="javascript:void(0)">%2$s</a>',
+            '<span data-id="' . $item['id'] . '" class="dashicons dashicons-menu handle" style="cursor:all-scroll;"></span>',
+            ucwords($item['name']),
+        );
+    }
+
+    function column_cb($item)
+    {
+        return '';
+    }
+
+    function get_columns()
+    {
+
+        $columns = array(
+            'status_id' => __('Status', 'aes'),
+            'alliance_id' => __('Alliance', 'aes'),
+            'month' => __('Month', 'aes'),
+            'amount' => __('Amount', 'aes'),
+            'total_orders' => __('Total Orders', 'aes'),
+            'view_details' => __('Actions', 'aes'),
+        );
+
+        return $columns;
+    }
+
+    function get_invoices_alliances()
+    {
+        global $wpdb;
+        $per_page = 20;
+        $pagenum = isset($_GET['paged']) ? absint($_GET['paged']) : 1;
+        $offset = (($pagenum - 1) * $per_page);
+
+        $table_alliances_payments = $wpdb->prefix . 'alliances_payments';
+        $transactions = $wpdb->get_results("SELECT * FROM {$table_alliances_payments}");
+
+        return ['data' => $transactions, 'total_count' => sizeof($transactions)];
+    }
+
+    function get_sortable_columns()
+    {
+        $sortable_columns = [];
+        return $sortable_columns;
+    }
+
+
+    function get_bulk_actions()
+    {
+        $actions = [];
+        return $actions;
+    }
+
+    function process_bulk_action()
+    {
+
+        //Detect when a bulk action is being triggered...
+        if ('delete' === $this->current_action()) {
+            wp_die('Items deleted (or they would be if we had items to delete)!');
+        }
+    }
+
+    function prepare_items()
+    {
+
+        $data_invoices = $this->get_invoices_alliances();
+        $columns = $this->get_columns();
+        $hidden = array();
+        $sortable = $this->get_sortable_columns();
+
+        $this->_column_headers = array($columns, $hidden, $sortable);
+        $this->process_bulk_action();
+        $data = $data_invoices['data'];
+        $total_count = (int) $data_invoices['total_count'];
 
         function usort_reorder($a, $b)
         {
