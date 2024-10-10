608ed7402d4fc917a276d430dd46d84700970e20
Junnior Rivas <jrdrivasg@gmail.com>
2024-10-10 10:42:26 -0400

    fix

--- public/templates/fill-info.php
+++ public/templates/fill-info.php
@@ -2,10 +2,15 @@
 
 <?php global $current_user ?>
 <div class="modal-content modal-enrollment" id="modal-content">
-    <div id="modal-contraseña" class="modal" style="overflow: auto; padding: 0 !important">
-        <div class="modal-content modal-enrollment">
+    <div id="modal-contraseña" class="modal" >
+        <div class="modal-content modal-enrollment" style="overflow: auto; padding: 0 !important">
             <!-- <span id="close-modal-enrollment" style="float: right; cursor: pointer"><span
                     class='dashicons dashicons-no-alt'></span></span> -->
+            <div class="modal-header">
+                <div class="title" style="color: #4faf4f">
+                    Payment successfully
+                </div>
+            </div>
             <div class="modal-body" id="content-pdf">
                 <form method="POST" action="<?php the_permalink(); ?>?action=save_student_custom" class="form-aes">
                     <input class="formdata" autocomplete="off" type="hidden" id="modal_open" name="modal_open"
@@ -16,8 +21,8 @@
                         <div class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6">
                             <div>
                                 <p>Welcome to <strong>American Elite School!</strong></p>
-                                <p class="color-success">Payment successfully</p>
-                                <p>We're thrilled to have you on board! To ensure the security of your account, we require you to set a password before accessing your account.</p>
+                                <p>We're thrilled to have you on board! To ensure the security of your account, we
+                                    require you to set a password before accessing your account.</p>
                             </div>
                             <div>
                                 <label for="password"><?= __('Password of access', 'aes'); ?><span
