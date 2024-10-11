7004d1b5e057e68f41f80f61a557e6bef26af783
Junnior Rivas <jrdrivasg@gmail.com>
2024-10-11 14:04:10 -0400

    fx

--- settings.php
+++ settings.php
@@ -9,7 +9,7 @@
 	define( 'AES_DUAL_10MO_VARIABLE','57'); // Product variable
 	define( 'AES_DUAL_DEFAULT', '53' );    // Product initial 11 and bacheloder
 	define( 'AES_DUAL_DEFAULT_VARIABLE','60'); // Product variable
-	define( 'ROLES_OF_STAFF', array('owner', 'administrador', 'admision', 'admission', 'alliance', 'administration', 'allied'));
+	define( 'ROLES_OF_STAFF', array('owner', 'administrador', 'admision', 'admission', 'alliance', 'administration', 'allied', 'webinar-aliance'));
 
 	// FOR ALL USERS, DONT COMMENT
 	define( 'AES_PROGRAM_ID','HSD-01'); // AES program for endpoint for laravel create user and move documents
