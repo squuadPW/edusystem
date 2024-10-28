223d86691c84fb4b9cb3881246b99c592cf8a40e
Junnior Rivas <jrdrivasg@gmail.com>
2024-10-28 17:32:37 -0400

    fix

--- public/assets/js/student-unsubscribe.js
+++ public/assets/js/student-unsubscribe.js
@@ -8,6 +8,8 @@ document.addEventListener('DOMContentLoaded',function(){
 
     if(document.getElementById('send-unsubscribe')){
       document.getElementById('send-unsubscribe').addEventListener('click', function(event) {
+        document.getElementById('send-unsubscribe').disabled = true;
+        document.getElementById('send-unsubscribe').innerText = 'Loading...';
         let reason = document.querySelector('textarea[name="reason"]').value;
         const XHR = new XMLHttpRequest();
         XHR.open("POST", `${ajax_object.ajax_url}?action=student_unsubscribe`, true);
@@ -21,6 +23,9 @@ document.addEventListener('DOMContentLoaded',function(){
         XHR.onload = function () {
           if (XHR.status === 200) {
             location.reload();
+          } else {
+            document.getElementById('send-unsubscribe').disabled = false;
+            document.getElementById('send-unsubscribe').innerText = 'Unsubscribe';
           }
         };
       });
