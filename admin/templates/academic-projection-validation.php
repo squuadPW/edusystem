ea5d3d18615ed65248505840ec98e6a8f732fd83
Jose Mora <jmoraw7@gmail.com>
2024-12-02 12:23:44 -0400

    fixes

--- admin/templates/academic-projection-validation.php
+++ admin/templates/academic-projection-validation.php
@@ -1,54 +1,54 @@
 <div>
     <div>
-        <h4>ESTUDIANTES EN HISTORIA</h4>
+        <h4>ESTUDIANTES EN HISTORIA (<?= count($history) ?>)</h4>
         <ul>
             <?php foreach ($history as $key => $value) { ?>
-                <li><?= $value->name . ' ' . $value->middle_name . ' ' . $value->last_name . ' ' . $value->middle_last_name ?></li>
+                <li><?= strtoupper($value->last_name) . ' ' . strtoupper($value->middle_last_name) . ' ' . strtoupper($value->name) . ' ' . strtoupper($value->middle_name) ?></li>
             <?php } ?>
         </ul>
     </div>
     <br>
     <div>
-        <h4>ESTUDIANTES EN GOBIERNO</h4>
+        <h4>ESTUDIANTES EN GOBIERNO (<?= count($government) ?>)</h4>
         <ul>
             <?php foreach ($government as $key => $value) { ?>
-                <li><?= $value->name . ' ' . $value->middle_name . ' ' . $value->last_name . ' ' . $value->middle_last_name ?></li>
+                <li><?= strtoupper($value->last_name) . ' ' . strtoupper($value->middle_last_name) . ' ' . strtoupper($value->name) . ' ' . strtoupper($value->middle_name) ?></li>
             <?php } ?>
         </ul>
     </div>
     <br>
     <div>
-        <h4>ESTUDIANTES EN INGLES III</h4>
+        <h4>ESTUDIANTES EN INGLES III (<?= count($english_tree) ?>)</h4>
         <ul>
             <?php foreach ($english_tree as $key => $value) { ?>
-                <li><?= $value->name . ' ' . $value->middle_name . ' ' . $value->last_name . ' ' . $value->middle_last_name ?></li>
+                <li><?= strtoupper($value->last_name) . ' ' . strtoupper($value->middle_last_name) . ' ' . strtoupper($value->name) . ' ' . strtoupper($value->middle_name) ?></li>
             <?php } ?>
         </ul>
     </div>
     <br>    
     <div>
-        <h4>ESTUDIANTES EN INGLES IV</h4>
+        <h4>ESTUDIANTES EN INGLES IV (<?= count($english_four) ?>)</h4>
         <ul>
             <?php foreach ($english_four as $key => $value) { ?>
-                <li><?= $value->name . ' ' . $value->middle_name . ' ' . $value->last_name . ' ' . $value->middle_last_name ?></li>
+                <li><?= strtoupper($value->last_name) . ' ' . strtoupper($value->middle_last_name) . ' ' . strtoupper($value->name) . ' ' . strtoupper($value->middle_name) ?></li>
             <?php } ?>
         </ul>
     </div>
     <br>
     <div>
-        <h4>ESTUDIANTES EN ECONOMIA</h4>
+        <h4>ESTUDIANTES EN ECONOMIA (<?= count($economic) ?>)</h4>
         <ul>
             <?php foreach ($economic as $key => $value) { ?>
-                <li><?= $value->name . ' ' . $value->middle_name . ' ' . $value->last_name . ' ' . $value->middle_last_name ?></li>
+                <li><?= strtoupper($value->last_name) . ' ' . strtoupper($value->middle_last_name) . ' ' . strtoupper($value->name) . ' ' . strtoupper($value->middle_name) ?></li>
             <?php } ?>
         </ul>
     </div>
     <br>
     <div>
-        <h4>ESTUDIANTES EN PRE-CALCULO</h4>
+        <h4>ESTUDIANTES EN PRE-CALCULO (<?= count($precalc) ?>)</h4>
         <ul>
             <?php foreach ($precalc as $key => $value) { ?>
-                <li><?= $value->name . ' ' . $value->middle_name . ' ' . $value->last_name . ' ' . $value->middle_last_name ?></li>
+                <li><?= strtoupper($value->last_name) . ' ' . strtoupper($value->middle_last_name) . ' ' . strtoupper($value->name) . ' ' . strtoupper($value->middle_name) ?></li>
             <?php } ?>
         </ul>
     </div>
