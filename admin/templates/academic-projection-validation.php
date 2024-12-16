21206c929457046340eb6c27d51077783e7c9389
Jose Mora <jmoraw7@gmail.com>
2024-12-16 12:12:06 -0400

    minor fix

--- admin/templates/academic-projection-validation.php
+++ admin/templates/academic-projection-validation.php
@@ -1,56 +1,122 @@
 <div>
-    <div>
-        <h4>ESTUDIANTES EN HISTORIA (<?= count($history) ?>)</h4>
-        <ul>
-            <?php foreach ($history as $key => $value) { ?>
-                <li><?= strtoupper($value->last_name) . ' ' . strtoupper($value->middle_last_name) . ' ' . strtoupper($value->name) . ' ' . strtoupper($value->middle_name) ?></li>
-            <?php } ?>
-        </ul>
+    <div style="padding: 20px !important">
+        <table class="wp-list-table widefat fixed posts striped">
+            <thead>
+                <tr>
+                    <th>ESTUDIANTES EN HISTORIA (<?= count($history) ?>)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($history as $key => $value) { ?>
+
+                    <tr>
+                        <td>
+                            <?= strtoupper($value->last_name) . ' ' . strtoupper($value->middle_last_name) . ' ' . strtoupper($value->name) . ' ' . strtoupper($value->middle_name) ?>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
     </div>
     <br>
-    <div>
-        <h4>ESTUDIANTES EN GOBIERNO (<?= count($government) ?>)</h4>
-        <ul>
-            <?php foreach ($government as $key => $value) { ?>
-                <li><?= strtoupper($value->last_name) . ' ' . strtoupper($value->middle_last_name) . ' ' . strtoupper($value->name) . ' ' . strtoupper($value->middle_name) ?></li>
-            <?php } ?>
-        </ul>
+    <div style="padding: 20px !important">
+        <table class="wp-list-table widefat fixed posts striped">
+            <thead>
+                <tr>
+                    <th>ESTUDIANTES EN GOBIERNO (<?= count($government) ?>)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($government as $key => $value) { ?>
+
+                    <tr>
+                        <td>
+                            <?= strtoupper($value->last_name) . ' ' . strtoupper($value->middle_last_name) . ' ' . strtoupper($value->name) . ' ' . strtoupper($value->middle_name) ?>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
     </div>
     <br>
-    <div>
-        <h4>ESTUDIANTES EN INGLES III (<?= count($english_tree) ?>)</h4>
-        <ul>
-            <?php foreach ($english_tree as $key => $value) { ?>
-                <li><?= strtoupper($value->last_name) . ' ' . strtoupper($value->middle_last_name) . ' ' . strtoupper($value->name) . ' ' . strtoupper($value->middle_name) ?></li>
-            <?php } ?>
-        </ul>
-    </div>
-    <br>    
-    <div>
-        <h4>ESTUDIANTES EN INGLES IV (<?= count($english_four) ?>)</h4>
-        <ul>
-            <?php foreach ($english_four as $key => $value) { ?>
-                <li><?= strtoupper($value->last_name) . ' ' . strtoupper($value->middle_last_name) . ' ' . strtoupper($value->name) . ' ' . strtoupper($value->middle_name) ?></li>
-            <?php } ?>
-        </ul>
-    </div>
-    <br>
-    <div>
-        <h4>ESTUDIANTES EN ECONOMIA (<?= count($economic) ?>)</h4>
-        <ul>
-            <?php foreach ($economic as $key => $value) { ?>
-                <li><?= strtoupper($value->last_name) . ' ' . strtoupper($value->middle_last_name) . ' ' . strtoupper($value->name) . ' ' . strtoupper($value->middle_name) ?></li>
-            <?php } ?>
-        </ul>
+    <div style="padding: 20px !important">
+        <table class="wp-list-table widefat fixed posts striped">
+            <thead>
+                <tr>
+                    <th>ESTUDIANTES EN INGLES III (<?= count($english_tree) ?>)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($english_tree as $key => $value) { ?>
+
+                    <tr>
+                        <td>
+                            <?= strtoupper($value->last_name) . ' ' . strtoupper($value->middle_last_name) . ' ' . strtoupper($value->name) . ' ' . strtoupper($value->middle_name) ?>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
     </div>
     <br>
-    <div>
-        <h4>ESTUDIANTES EN PRE-CALCULO (<?= count($precalc) ?>)</h4>
-        <ul>
-            <?php foreach ($precalc as $key => $value) { ?>
-                <li><?= strtoupper($value->last_name) . ' ' . strtoupper($value->middle_last_name) . ' ' . strtoupper($value->name) . ' ' . strtoupper($value->middle_name) ?></li>
-            <?php } ?>
-        </ul>
+    <div style="padding: 20px !important">
+        <table class="wp-list-table widefat fixed posts striped">
+            <thead>
+                <tr>
+                    <th>ESTUDIANTES EN INGLES IV (<?= count($english_four) ?>)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($english_four as $key => $value) { ?>
+
+                    <tr>
+                        <td>
+                            <?= strtoupper($value->last_name) . ' ' . strtoupper($value->middle_last_name) . ' ' . strtoupper($value->name) . ' ' . strtoupper($value->middle_name) ?>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+    <br>
+    <div style="padding: 20px !important">
+       <table class="wp-list-table widefat fixed posts striped">
+            <thead>
+                <tr>
+                    <th>ESTUDIANTES EN ECONOMIA (<?= count($economic) ?>)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($economic as $key => $value) { ?>
+
+                    <tr>
+                        <td>
+                            <?= strtoupper($value->last_name) . ' ' . strtoupper($value->middle_last_name) . ' ' . strtoupper($value->name) . ' ' . strtoupper($value->middle_name) ?>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+    <br>
+    <div style="padding: 20px !important">
+        <table class="wp-list-table widefat fixed posts striped">
+            <thead>
+                <tr>
+                    <th>ESTUDIANTES EN PRE-CALCULO (<?= count($precalc) ?>)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($precalc as $key => $value) { ?>
+
+                    <tr>
+                        <td>
+                            <?= strtoupper($value->last_name) . ' ' . strtoupper($value->middle_last_name) . ' ' . strtoupper($value->name) . ' ' . strtoupper($value->middle_name) ?>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
     </div>
     <br>
 </div>
