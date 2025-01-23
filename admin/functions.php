13472c41c4a0be0cbabba30defe009351d2ec88f
Jose Mora <jmoraw7@gmail.com>
2025-01-23 11:38:12 -0400

    fixes

--- admin/functions.php
+++ admin/functions.php
@@ -539,8 +539,8 @@ function save_num_cuotes($variation_id, $i)
 
 function add_logo_dashboard()
 {
-    $url = 'http://online.american-elite.us/wp-content/uploads/2024/07/cropped-cropped-cropped-LOGOS-Y-ONDAS-10.png';
-    $url_small = 'http://online.american-elite.us/wp-content/uploads/2024/07/cropped-cropped-cropped-LOGOS-Y-ONDAS-10.png';
+    $url = 'https://online.american-elite.us/wp-content/uploads/2024/07/cropped-cropped-cropped-LOGOS-Y-ONDAS-10.png';
+    $url_small = 'https://online.american-elite.us/wp-content/uploads/2024/07/cropped-cropped-cropped-LOGOS-Y-ONDAS-10.png';
     echo '<style>
     @media screen and (min-width:992px){
         #toplevel_page_logo_based_menu {
@@ -592,7 +592,7 @@ add_action('admin_enqueue_scripts', 'add_logo_dashboard');
 function custom_login_store()
 {
     if (get_option('blog_img_logo')) {
-        $url = 'http://online.american-elite.us/wp-content/uploads/2024/07/cropped-cropped-cropped-LOGOS-Y-ONDAS-10.png';
+        $url = 'https://online.american-elite.us/wp-content/uploads/2024/07/cropped-cropped-cropped-LOGOS-Y-ONDAS-10.png';
         echo '
     <style type="text/css">
         #login h1 a, .login h1 a {
