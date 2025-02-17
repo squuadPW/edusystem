45925dbbaca7484f7066126c1a41f63703340519
Jose Mora <jmoraw7@gmail.com>
2025-02-17 09:44:32 -0400

    fixes

--- public/templates/student-access-classroom.php
+++ public/templates/student-access-classroom.php
@@ -9,8 +9,8 @@
         <h4 style="font-size:18px;text-align:center; margin-bottom: 20px"><?= __('Access','aes'); ?></h4>
     <?php } ?>
     <div style="display:flex">
-        <div style="width: 100%; text-align: end">
-            <a style="width: 70% !important; <?php echo count($access) == 0 ? 'background-color: #091c5c75 !important; pointer-events: none;' : '' ?>" target="_blank" href="<?= home_url('?action=access_moodle_url&student_id='.$data->id); ?>" class="button button-primary"><?= __('Virtual Classroom','aes'); ?></a>
+        <div style="width: 100%; text-align: center">
+            <a style="width: 60% !important; <?php echo count($access) == 0 ? 'background-color: #091c5c75 !important; pointer-events: none;' : '' ?>" target="_blank" href="<?= home_url('?action=access_moodle_url&student_id='.$data->id); ?>" class="button button-primary"><?= __('Virtual Classroom','aes'); ?></a>
         </div>
         <!-- <div style="width: 50%; text-align: start">
             <a style="width: 70% !important; <?php echo count($access) == 0 ? 'background-color: #091c5c75 !important; pointer-events: none;' : '' ?>" target="_blank" href="<?= $url.'login-student?user='.$data->id_document; ?>" class="button button-primary"><?= __('Student area','aes'); ?></a>
