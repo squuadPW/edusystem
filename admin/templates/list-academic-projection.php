fa9f752c6e877d71ef4b94e2cb6bab1cdd462c61
Jose Mora <jmoraw7@gmail.com>
2025-02-21 12:55:42 -0400

    fixes

--- admin/templates/list-academic-projection.php
+++ admin/templates/list-academic-projection.php
@@ -15,9 +15,14 @@
 			</div>
 			<?php setcookie('message-error', '', time(), '/'); ?>
 		<?php } ?>
+		<?php 
+			if ($enroll_moodle_count > 0) {
+				$style_pending = 'background-color: #2271b1; color: white;';
+			}
+		?>
 		<div style="display:flex;width:100%;justify-content:end;margin-bottom:10px;">
 			<a href="<?= admin_url('admin.php?page=add_admin_form_academic_projection_content&section_tab=validate_enrollments'); ?>" class="button button-outline-primary" onclick="return confirm('Are you sure you want to validate enrollments?');"><?= __('Enrollment history','aes'); ?></a>
-			<a style="margin-left: 10px" href="<?= admin_url('admin.php?page=add_admin_form_academic_projection_content&action=generate_enrollments_moodle'); ?>" class="button button-outline-primary" onclick="return confirm('Are you sure you want to generate enrollments in Moodle?');"><?= __('Enroll in moodle','aes'); ?> (<?= $enroll_moodle_count ?>)</a>
+			<a style="margin-left: 10px; <?= $style_pending ?>" href="<?= admin_url('admin.php?page=add_admin_form_academic_projection_content&action=generate_enrollments_moodle'); ?>" class="button button-outline-primary" onclick="return confirm('Are you sure you want to generate enrollments in Moodle?');"><?= __('Enroll in moodle','aes'); ?> (<?= $enroll_moodle_count ?>)</a>
 			<a style="margin-left: 10px" href="<?= admin_url('admin.php?page=add_admin_form_academic_projection_content&action=get_moodle_notes'); ?>" class="button button-outline-primary" onclick="return confirm('Are you sure you want to update the notes from Moodle?');"><?= __('Download moodle notes','aes'); ?></a>
 		</div>
 		<form action="" id="post-filter" method="get">
