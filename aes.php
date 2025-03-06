9c9b3f9b5eb8202b67578cd2b8edae69c34838fa
Jose Mora <jmoraw7@gmail.com>
2025-03-06 15:51:58 -0400

    version

--- aes.php
+++ aes.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Educational system
 Description: The WordPress plugin for educational system is a customized tool that offers a range of functionalities for the proper functioning of the institute website
-Version: 1.5.74
+Version: 1.5.75
 Author: EduSof
 Author URI: https://edusof.com/
 License:      GPL2
