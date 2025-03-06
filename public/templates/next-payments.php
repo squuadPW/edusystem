6bf8c264f046db67796465700f020b6fa197068b
Jose Mora <jmoraw7@gmail.com>
2025-03-06 15:59:42 -0400

    fixes

--- public/templates/next-payments.php
+++ public/templates/next-payments.php
@@ -5,7 +5,7 @@
         <table class="woocommerce-orders-table woocommerce-MyAccount-orders shop_table shop_table_responsive my_account_orders account-orders-table">
             <thead>
                     <tr>
-                        <th class="woocommerce-orders-table__header woocommerce-orders-table__header-order-date"><span class="nobr">Payment number</span></th>
+                        <th class="woocommerce-orders-table__header woocommerce-orders-table__header-order-quota"><span class="nobr">Quota</span></th>
                         <th class="woocommerce-orders-table__header woocommerce-orders-table__header-order-date"><span class="nobr">Date</span></th>
                         <th class="woocommerce-orders-table__header woocommerce-orders-table__header-order-total"><span class="nobr">Total</span></th>
                     </tr>
@@ -13,7 +13,7 @@
             <tbody>
                 <?php foreach ($payments as $payment) { ?>
                     <tr class="woocommerce-orders-table__row woocommerce-orders-table__row--status-pending">
-                        <td class="woocommerce-orders-table__cell woocommerce-orders-table__cell-order-total" data-title="Total"><?php echo $payment->cuote ?></td>
+                        <td class="woocommerce-orders-table__cell woocommerce-orders-table__cell-order-quota" data-title="Cuote"><?php echo $payment->cuote ?></td>
                         <td class="woocommerce-orders-table__cell woocommerce-orders-table__cell-order-date" data-title="Date"><?php echo date('F d, Y', strtotime($payment->date_next_payment)) ?></td>
                         <td class="woocommerce-orders-table__cell woocommerce-orders-table__cell-order-total" data-title="Total"><?php echo wc_price($payment->amount) ?></td>
                     </tr>
