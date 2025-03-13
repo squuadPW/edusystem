2b1f2d737f58da3ac59d9418f24a40230125388a
Jose Mora <jmoraw7@gmail.com>
2025-03-13 15:12:13 -0400

    fixes

--- public/assets/js/create-enrollment.js
+++ public/assets/js/create-enrollment.js
@@ -437,9 +437,9 @@ function sendSignatures(doc = null) {
 
   XHR.onload = function () {
     if (XHR.status === 200) {
-      document.getElementById("modal-contraseña").style.display = "none";
-      document.getElementById("modal-content").style.display = "none";
-      document.body.classList.remove("modal-open");
+      // document.getElementById("modal-contraseña").style.display = "none";
+      // document.getElementById("modal-content").style.display = "none";
+      // document.body.classList.remove("modal-open");
 
       location.reload();
     }
