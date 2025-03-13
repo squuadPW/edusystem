cbfb2bb75f191c68917167f386896c0d328b1340
Jose Mora <jmoraw7@gmail.com>
2025-03-13 12:38:29 -0400

    fixes

--- admin/admission.php
+++ admin/admission.php
@@ -1098,7 +1098,8 @@ function get_document_details($document_id) {
 
 function build_status_description($status_id, $description, $document) {
     $status_map = [
-        3 => 'Document rejected: '
+        3 => 'Document rejected: ',
+        5 => 'Document approved'
     ];
     
     return $status_map[$status_id] . ($description ?: $document->description);
