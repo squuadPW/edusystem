41484e63ca8057f0ee2157a0cb5ffa8ece6eff2a
Jose Mora <jmoraw7@gmail.com>
2025-05-08 15:39:19 -0400

    fixes

--- admin/templates/connections-student.php
+++ admin/templates/connections-student.php
@@ -6,17 +6,19 @@
 
 <?php if($student_id) { ?>
     <div style="margin-left: 5px; margin-right: 5px;">
-        <?php if($projection && current_user_can('manager_academic_projection_aes')) { ?>
+        <?php if($projection && current_user_can('manager_academic_projection_aes') && $_GET['page'] != 'add_admin_form_academic_projection_content') { ?>
             <a href="<?= admin_url('admin.php?page=add_admin_form_academic_projection_content&section_tab=academic_projection_details&projection_id=') . $projection->id ?>"
                 class="button button-outline-primary">
                 <?= __('Academic projection', 'edusystem'); ?>
             </a>
         <?php } ?>
+        <?php if($_GET['page'] != 'add_admin_form_admission_content') { ?>
         <a href="<?= admin_url('admin.php?page=add_admin_form_admission_content&section_tab=student_details&student_id=') . $student_id; ?>"
             class="button button-outline-primary">
             <?= __('Admission', 'edusystem'); ?>
         </a>
-        <?php if (current_user_can('manager_payments_aes')) { ?>
+        <?php } ?>
+        <?php if (current_user_can('manager_payments_aes') && $_GET['page'] != 'add_admin_form_payments_content') { ?>
             <a href="<?= admin_url('admin.php?page=add_admin_form_payments_content&section_tab=generate_advance_payment&student_available=1&id_document=') . $email ?>"
                 class="button button-outline-primary">
                 <?= __('Payments', 'edusystem'); ?>
