e1414f4ca3d8da953a13a8006db8be37f7ba75ac
Jose Mora <jmoraw7@gmail.com>
2025-05-14 10:04:29 -0400

    version

--- admin/alliance/institutes-registered.php
+++ admin/alliance/institutes-registered.php
@@ -1,32 +1,40 @@
 <?php
 
-function list_admin_institutes_partner_registered_content(){
+function list_admin_institutes_partner_registered_content()
+{
 
-    if(isset($_GET['action']) && !empty($_GET['action'])){
+    if (isset($_GET['action']) && !empty($_GET['action'])) {
 
-        if($_GET['action'] == 'institute-detail'){
+        if ($_GET['action'] == 'institute-detail') {
 
             $institute_id = $_GET['institute_id'];
             $institute = get_institute_details($institute_id);
             $countries = get_countries();
             $states = get_states_by_country_code($institute->country);
-            include(plugin_dir_path(__FILE__).'../templates/institute-details-alliance.php');
+            include(plugin_dir_path(__FILE__) . '../templates/institute-details-alliance.php');
 
-        }else if($_GET['action'] == 'institute-students'){
+        } else if ($_GET['action'] == 'institute-students') {
 
             $list_students_institute = new TT_Institute_Students_List_Table;
             $list_students_institute->prepare_items();
 
-            include(plugin_dir_path(__FILE__).'../templates/institute-students-alliance.php');
+            include(plugin_dir_path(__FILE__) . '../templates/institute-students-alliance.php');
 
-        }else if($_GET['action'] == 'add_institute'){
+        } else if ($_GET['action'] == 'institute-students-payments') {
+            global $wpdb;
+            $table_student_payments = $wpdb->prefix . 'student_payments';
+            $student = get_student_detail($_GET['student_id']);
+            $institute = get_institute_details($_GET['institute_id']);
+            $payments = $wpdb->get_results("SELECT * FROM {$table_student_payments} WHERE student_id='{$_GET['student_id']}' ORDER BY cuote ASC");
+            include(plugin_dir_path(__FILE__) . '../templates/institute-students-payments-alliance.php');
+        } else if ($_GET['action'] == 'add_institute') {
             $countries = get_countries();
-            include(plugin_dir_path(__FILE__).'../templates/institute-details-alliance.php');
+            include(plugin_dir_path(__FILE__) . '../templates/institute-details-alliance.php');
 
-        }else if($_GET['action'] == 'save_institute_details'){
-            
+        } else if ($_GET['action'] == 'save_institute_details') {
+
             global $wpdb, $current_user;
-            $table_institutes =  $wpdb->prefix.'institutes';
+            $table_institutes = $wpdb->prefix . 'institutes';
             $institute_id = $_POST['institute_id'];
 
             $name = $_POST['name'];
@@ -57,7 +65,7 @@ function list_admin_institutes_partner_registered_content(){
             $alliance_id = $alliance->id;
 
             //update
-            if(isset($institute_id) && !empty($institute_id)){
+            if (isset($institute_id) && !empty($institute_id)) {
 
                 $wpdb->update($table_institutes, [
                     'name' => $name,
@@ -85,13 +93,13 @@ function list_admin_institutes_partner_registered_content(){
                     'alliance_id' => $alliance_id,
                     'updated_at' => date('Y-m-d H:i:s')
                 ], ['id' => $institute_id]);
-                
-                setcookie('message',__('Changes saved successfully.','edusystem'),time() + 3600,'/');
-                wp_redirect(admin_url('admin.php?page=list_admin_institutes_partner_registered_content&action=institute-detail&institute_id='.$institute_id));
+
+                setcookie('message', __('Changes saved successfully.', 'edusystem'), time() + 3600, '/');
+                wp_redirect(admin_url('admin.php?page=list_admin_institutes_partner_registered_content&action=institute-detail&institute_id=' . $institute_id));
                 exit;
 
-            //insert
-            }else{
+                //insert
+            } else {
 
                 $wpdb->insert($table_institutes, [
                     'name' => $name,
@@ -124,29 +132,29 @@ function list_admin_institutes_partner_registered_content(){
 
                 $institute_id = $wpdb->insert_id;
                 $data_institute = $wpdb->get_row("SELECT * FROM {$table_institutes} WHERE id={$institute_id}");
-               
+
                 wp_redirect(admin_url('admin.php?page=list_admin_institutes_partner_registered_content'));
                 exit;
-        
+
             }
-        }else if($_GET['action'] == 'delete_institute'){
+        } else if ($_GET['action'] == 'delete_institute') {
 
             global $wpdb;
-            $table_institutes =  $wpdb->prefix.'institutes';
+            $table_institutes = $wpdb->prefix . 'institutes';
 
             $institute_id = $_POST['delete_institute_id'];
             $data_institute = $wpdb->get_row("SELECT * FROM {$table_institutes} WHERE id={$institute_id}");
-            $user = get_user_by('email',$data_institute->email);
-            
-            if($user){
+            $user = get_user_by('email', $data_institute->email);
+
+            if ($user) {
                 wp_delete_user($user->ID);
             }
 
-            $wpdb->delete($table_institutes,['id' => $institute_id]);
+            $wpdb->delete($table_institutes, ['id' => $institute_id]);
 
             wp_redirect(admin_url('admin.php?page=list_admin_institutes_partner_registered_content'));
             exit;
-        }else if($_GET['action'] == 'fee-institute'){
+        } else if ($_GET['action'] == 'fee-institute') {
 
             global $current_user;
             $roles = $current_user->roles;
@@ -154,95 +162,103 @@ function list_admin_institutes_partner_registered_content(){
             $institute = get_institute_details($institute_id);
             $list_payment_institutes = new TT_all_Payment_Institutes_List_Table;
             $list_payment_institutes->prepare_items();
-            include(plugin_dir_path(__FILE__).'../templates/list-payments-institutes.php');
+            include(plugin_dir_path(__FILE__) . '../templates/list-payments-institutes.php');
         }
 
-    }else{
-        
+    } else {
+
         $list_institutes_alliance = new TT_all_Institutes_Alliance_List_Table;
         $list_institutes_alliance->prepare_items();
-        include(plugin_dir_path(__FILE__).'../templates/list-institutes-alliance.php');
+        include(plugin_dir_path(__FILE__) . '../templates/list-institutes-alliance.php');
     }
 }
 
-class TT_all_Institutes_Alliance_List_Table extends WP_List_Table{
+class TT_all_Institutes_Alliance_List_Table extends WP_List_Table
+{
 
-	function __construct(){
-        global $status, $page,$categories;
-         
-        parent::__construct( array(
-            'singular'  => 'institute',    
-            'plural'    => 'institutes',
-            'ajax'      => true
-        ) );
+    function __construct()
+    {
+        global $status, $page, $categories;
+
+        parent::__construct(array(
+            'singular' => 'institute',
+            'plural' => 'institutes',
+            'ajax' => true
+        ));
     }
 
-	function column_default($item, $column_name){
+    function column_default($item, $column_name)
+    {
 
         global $current_user;
 
-        switch($column_name){
+        switch ($column_name) {
             case 'name':
-                return $item['name'].' '.$item['last_name'];
+                return $item['name'] . ' ' . $item['last_name'];
             case 'email':
                 return $item['email'];
             case 'level':
                 return get_name_level($item['level_id']);
             case 'status':
                 return get_name_status_alliance($item['status']);
-            case 'created_at':   
-                $datetime = Datetime::createFromFormat('Y-m-d H:i:s',$item['created_at']);
+            case 'created_at':
+                $datetime = Datetime::createFromFormat('Y-m-d H:i:s', $item['created_at']);
 
                 return $datetime->format('F j, Y');
             case 'view_details':
-                if($item['status'] == 1){
-                    $buttons = "<a class='button button-primary' href='".admin_url('admin.php?page=list_admin_institutes_partner_registered_content&action=fee-institute&institute_id='.$item['id'])."'><span class='dashicons dashicons-money-alt'></span>".__('Fees','edusystem')."</a>";
+                if ($item['status'] == 1) {
+                    $buttons = "<a class='button button-primary' href='" . admin_url('admin.php?page=list_admin_institutes_partner_registered_content&action=fee-institute&institute_id=' . $item['id']) . "'><span class='dashicons dashicons-money-alt'></span>" . __('Fees', 'edusystem') . "</a>";
                     // $buttons .= "<a style='margin-left: 10px' class='button button-primary' href='".admin_url('admin.php?page=list_admin_institutes_partner_registered_content&action=institute-detail&institute_id='.$item['id'])."'><span class='dashicons dashicons-edit'></span>".__('Edit','edusystem')."</a>";
-                    $buttons .= "<a style='margin-left: 4px' class='button button-primary' href='".admin_url('admin.php?page=list_admin_institutes_partner_registered_content&action=institute-students&institute_id='.$item['id'])."'><span class='dashicons dashicons-admin-users'></span>".__('Students','edusystem')."</a>";
+                    $buttons .= "<a style='margin-left: 4px' class='button button-primary' href='" . admin_url('admin.php?page=list_admin_institutes_partner_registered_content&action=institute-students&institute_id=' . $item['id']) . "'><span class='dashicons dashicons-admin-users'></span>" . __('Students', 'edusystem') . "</a>";
                     return $buttons;
-                }else{
+                } else {
                     // return "<a class='button button-primary' href='".admin_url('admin.php?page=list_admin_institutes_partner_registered_content&action=institute-detail&institute_id='.$item['id'])."'><span class='dashicons dashicons-edit'></span>".__('Edit','edusystem')."</a>";
                     return '';
                 }
-               
-			default:
-				return print_r($item,true);
+
+            default:
+                return print_r($item, true);
         }
     }
 
-	function column_name($item){
+    function column_name($item)
+    {
         return ucwords($item['name']);
     }
 
-	function column_cb($item){
+    function column_cb($item)
+    {
         return '';
     }
 
-	function get_columns(){
+    function get_columns()
+    {
 
         $columns = array(
-            'name'     => __('Name','edusystem'),
-            'email'    => __('Email','edusystem'),
-            'level'    => __('Level','edusystem'),
-            'status'   => __('Status','edusystem'),
-            'created_at' => __('Created at','edusystem'),
-            'view_details' => __('Actions','edusystem'),
+            'name' => __('Name', 'edusystem'),
+            'email' => __('Email', 'edusystem'),
+            'level' => __('Level', 'edusystem'),
+            'status' => __('Status', 'edusystem'),
+            'created_at' => __('Created at', 'edusystem'),
+            'view_details' => __('Actions', 'edusystem'),
         );
 
         return $columns;
     }
 
-    function get_institutes(){
+    function get_institutes()
+    {
 
         global $wpdb;
-        $table_institutes =  $wpdb->prefix.'institutes';
-        $alliance_id = get_user_meta(get_current_user_id(),'alliance_id',true);
+        $table_institutes = $wpdb->prefix . 'institutes';
+        $alliance_id = get_user_meta(get_current_user_id(), 'alliance_id', true);
 
-        if(isset($_GET['s']) && !empty($_GET['s'])){
+        if (isset($_GET['s']) && !empty($_GET['s'])) {
 
             $search = $_GET['s'];
 
-            $data = $wpdb->get_results("SELECT * FROM 
+            $data = $wpdb->get_results(
+                "SELECT * FROM 
                 {$table_institutes} 
                 WHERE alliance_id={$alliance_id}
                 AND 
@@ -251,63 +267,70 @@ class TT_all_Institutes_Alliance_List_Table extends WP_List_Table{
                     name_rector LIKE '{$search}%' || 
                     lastname_rector LIKE '{$search}%'
                 )"
-            ,"ARRAY_A");
+                ,
+                "ARRAY_A"
+            );
             return $data;
 
-        }else{
+        } else {
 
-            $data = $wpdb->get_results("SELECT * FROM {$table_institutes} WHERE alliance_id={$alliance_id}","ARRAY_A");
+            $data = $wpdb->get_results("SELECT * FROM {$table_institutes} WHERE alliance_id={$alliance_id}", "ARRAY_A");
             return $data;
-        }     
-    }   
+        }
+    }
 
-	function get_sortable_columns() {
+    function get_sortable_columns()
+    {
         $sortable_columns = [];
         return $sortable_columns;
     }
 
-	
-	function get_bulk_actions() {
+
+    function get_bulk_actions()
+    {
         $actions = [];
         return $actions;
     }
 
-	function process_bulk_action(){
-        
+    function process_bulk_action()
+    {
+
         //Detect when a bulk action is being triggered...
-        if( 'delete'===$this->current_action() ) {
+        if ('delete' === $this->current_action()) {
             wp_die('Items deleted (or they would be if we had items to delete)!');
-        }  
+        }
     }
 
-	function prepare_items(){
+    function prepare_items()
+    {
 
-		$data_categories = $this->get_institutes();
+        $data_categories = $this->get_institutes();
 
-		$per_page = 100;
+        $per_page = 100;
 
-		  
+
         $columns = $this->get_columns();
         $hidden = array();
         $sortable = $this->get_sortable_columns();
 
         $this->_column_headers = array($columns, $hidden, $sortable);
-		$this->process_bulk_action();
+        $this->process_bulk_action();
         $data = $data_categories;
 
-		function usort_reorder($a,$b){
+        function usort_reorder($a, $b)
+        {
             $orderby = (!empty($_REQUEST['orderby'])) ? $_REQUEST['orderby'] : 'order';
-            $order = (!empty($_REQUEST['order'])) ? $_REQUEST['order'] : 'asc'; 
-            $result = strcmp($a[$orderby], $b[$orderby]); 
-            return ($order==='asc') ? $result : -$result;
+            $order = (!empty($_REQUEST['order'])) ? $_REQUEST['order'] : 'asc';
+            $result = strcmp($a[$orderby], $b[$orderby]);
+            return ($order === 'asc') ? $result : -$result;
         }
-       
-		$current_page = $this->get_pagenum();
-    
+
+        $current_page = $this->get_pagenum();
+
         $total_items = count($data);
-		
+
         $this->items = $data;
-	}
+    }
 
 }
 
@@ -323,7 +346,8 @@ class TT_Institute_Students_List_Table extends WP_List_Table
                 'singular' => 'school_subject_',
                 'plural' => 'school_subject_s',
                 'ajax' => true
-            ));
+            )
+        );
 
     }
 
@@ -336,6 +360,10 @@ class TT_Institute_Students_List_Table extends WP_List_Table
             case 'initial':
                 $initial = $item['academic_period'] . ' - ' . $item['initial_cut'];
                 return $initial;
+            case 'payments':
+                $buttons = '';
+                $buttons .= "<a style='margin: 1px' href='" . admin_url('/admin.php?page=list_admin_institutes_partner_registered_content&action=institute-students-payments&student_id=' . $item['id'] . '&institute_id=' . $_GET['institute_id']) . "' class='button button-primary'>" . __('View payments', 'edusystem') . "</a>";
+                return $buttons;
             default:
                 return $item[$column_name];
         }
@@ -359,6 +387,7 @@ class TT_Institute_Students_List_Table extends WP_List_Table
             'email' => __('Email', 'edusystem'),
             'initial' => __('Initial term', 'edusystem'),
             'created_at' => __('Created at', 'edusystem'),
+            'payments' => __('Payments', 'edusystem'),
         );
 
         return $columns;
@@ -367,42 +396,41 @@ class TT_Institute_Students_List_Table extends WP_List_Table
     function get_students()
     {
         global $wpdb;
-        $students_array = [];
         $institute_id = isset($_GET['institute_id']) ? (int) $_GET['institute_id'] : '';
-    
+
         // PAGINATION
         $per_page = 20;
         $pagenum = isset($_GET['paged']) ? absint($_GET['paged']) : 1;
         $offset = (($pagenum - 1) * $per_page);
         // PAGINATION
-    
+
         $table_students = $wpdb->prefix . 'students';
-    
+
         // Construir WHERE dinámicamente
         $where_clauses = array();
         $params = array();
-    
+
         if (!empty($institute_id)) {
             $where_clauses[] = 'institute_id = %d';
             $params[] = $institute_id;
         }
-    
+
         $sql = "SELECT SQL_CALC_FOUND_ROWS * FROM {$table_students}";
-    
+
         if (!empty($where_clauses)) {
             $sql .= ' WHERE ' . implode(' AND ', $where_clauses);
         }
-    
+
         $sql .= ' ORDER BY id DESC LIMIT %d OFFSET %d';
         $params[] = $per_page;
         $params[] = $offset;
-    
+
         // Preparar y ejecutar consulta segura
         $prepared_sql = $wpdb->prepare($sql, $params);
         $students = $wpdb->get_results($prepared_sql, ARRAY_A);
-    
+
         $total_count = $wpdb->get_var("SELECT FOUND_ROWS()");
-    
+
         return ['data' => $students, 'total_count' => $total_count];
     }
 
@@ -441,7 +469,7 @@ class TT_Institute_Students_List_Table extends WP_List_Table
 
         $this->_column_headers = array($columns, $hidden, $sortable);
         $this->process_bulk_action();
-        
+
         $data = $data_get['data'];
         $total_count = (int) $data_get['total_count'];
 
---
+++ admin/templates/institute-students-payments-alliance.php
@@ -0,0 +1,118 @@
+<div class="tabs-content">
+	<div class="wrap">
+		<div style="text-align:start;">
+			<h1 class="wp-heading-line"><?= __('Payments of student', 'edusystem'); ?></h1>
+		</div>
+
+		<?php if (isset($_COOKIE['message']) && !empty($_COOKIE['message'])) { ?>
+			<div class="notice notice-success is-dismissible">
+				<p><?= $_COOKIE['message']; ?></p>
+			</div>
+			<?php setcookie('message', '', time(), '/'); ?>
+		<?php } ?>
+		<?php if (isset($_COOKIE['message-error']) && !empty($_COOKIE['message-error'])) { ?>
+			<div class="notice notice-error is-dismissible">
+				<p><?= $_COOKIE['message-error']; ?></p>
+			</div>
+			<?php setcookie('message-error', '', time(), '/'); ?>
+		<?php } ?>
+		<div style="display:flex;width:100%;">
+			<a class="button button-outline-primary"
+				href="<?= admin_url('admin.php?page=list_admin_institutes_partner_registered_content&action=institute-students&institute_id=' . $institute->id); ?>"><?= __('Back', 'edusystem'); ?></a>
+		</div>
+		<div>
+
+			<div style="padding: 10px">
+				<table class="wp-list-table widefat fixed posts striped">
+					<thead>
+						<tr>
+							<th>Student details</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td><span style="margin-top: -6px;" class='dashicons dashicons-businessperson'></span>
+								Name: <?php echo $student->name ?>
+								<?php echo $student->middle_name ?>
+								<?php echo $student->last_name ?>
+								<?php echo $student->middle_last_name ?>
+							</td>
+						</tr>
+						<tr>
+							<td><span style="margin-top: -6px;" class='dashicons dashicons-email'></span> Email:
+								<?php echo $student->email ?>
+							</td>
+						</tr>
+						<tr>
+							<td><span style="margin-top: -6px;" class='dashicons dashicons-phone'></span> Phone:
+								<?php echo $student->phone ?>
+							</td>
+						</tr>
+						<tr>
+							<td><span style="margin-top: -6px;" class='dashicons dashicons-location-alt'></span>
+								Country: <?php echo $student->country ?>
+							</td>
+						</tr>
+						<tr>
+							<td><span style="margin-top: -6px;" class='dashicons dashicons-text-page'></span>
+								Academic period: <?php echo $student->academic_period ?></td>
+						</tr>
+						<tr>
+							<td><span style="margin-top: -6px;" class='dashicons dashicons-location-alt'></span> Initial
+								cut: <?php echo $student->initial_cut ?>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+
+			<div style="padding: 10px">
+				<table class="wp-list-table widefat fixed posts striped">
+					<thead>
+						<tr>
+							<th colspan="4">Payment</th>
+							<th colspan="2" style="text-align: center;">Expected payment date</th>
+							<th colspan="2" style="text-align: center;">Date of payment made</th>
+							<th colspan="2" style="text-align: center;">Amount</th>
+							<th colspan="2" style="text-align: center;">Status</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php foreach ($payments as $payment) { ?>
+							<tr>
+								<td colspan="4">
+									<div style="display: flex; align-items: center; gap: 10px;">
+										<?php
+										$product = wc_get_product($payment->variation_id ?: $payment->product_id);
+										$name_product = $product->get_name();
+										?>
+										<span>#<?= $payment->cuote ?> - <?= $name_product ?></span>
+									</div>
+								</td>
+								<td colspan="2" style="text-align: center;">
+									<?=
+										date('m/d/Y', strtotime(in_array($payment->product_id, [FEE_INSCRIPTION, FEE_GRADUATION]) ? $payment->date_payment : $payment->date_next_payment))
+										?>
+								</td>
+								<td colspan="2" style="text-align: center;">
+									<?= $payment->status_id == 1 ? date('m/d/Y', strtotime($payment->date_payment)) : 'N/A'; ?>
+								</td>
+								<td colspan="2" style="text-align: center;">
+									<?=
+										wc_price($payment->amount) . '<input type="hidden" name="amount_payment[]" class="amount_payment" value="' . $payment->amount . '" />'
+										?>
+								</td>
+								<td colspan="2" style="text-align: center;">
+									<?= $payment->status_id == 1
+										? '<span style="color: green">Paid</span>'
+										: '<span style="color: gray">To pay</span>';
+									?>
+								</td>
+							</tr>
+						<?php } ?>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
