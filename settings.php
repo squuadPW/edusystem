4d0f4f515dbf0c49f350646ed027b0dfe7bba7fe
Jose Mora <jmoraw7@gmail.com>
2025-06-19 11:53:25 -0400

    fixes

--- settings.php
+++ settings.php
@@ -1,30 +1,30 @@
 <?php
 	//AWS
-	define('FEE_INSCRIPTION', '63'); // Product registration fee
-	define('FEE_GRADUATION', '1591'); // Product graduation fee
-	define('LATAM_SCHOLARSHIP', '62'); // Beca
-	define('DUAL_9NO', '51'); // Product Initial 9 Grade (lower)
-	define('DUAL_9NO_VARIABLE', '54'); // Product variable (lower)
-	define('DUAL_10MO', '52');// Product Initial 10mo Grade (middle)
-	define('DUAL_10MO_VARIABLE', '57'); // Product variable  (middle)
-	define('DUAL_DEFAULT', '53');    // Product initial 11 and bacheloder (upper)
-	define('DUAL_DEFAULT_VARIABLE','60'); // Product variable (upper)
+	// define('FEE_INSCRIPTION', '63'); // Product registration fee
+	// define('FEE_GRADUATION', '1591'); // Product graduation fee
+	// define('LATAM_SCHOLARSHIP', '62'); // Beca
+	// define('DUAL_9NO', '51'); // Product Initial 9 Grade (lower)
+	// define('DUAL_9NO_VARIABLE', '54'); // Product variable (lower)
+	// define('DUAL_10MO', '52');// Product Initial 10mo Grade (middle)
+	// define('DUAL_10MO_VARIABLE', '57'); // Product variable  (middle)
+	// define('DUAL_DEFAULT', '53');    // Product initial 11 and bacheloder (upper)
+	// define('DUAL_DEFAULT_VARIABLE','60'); // Product variable (upper)
 
 	// DREAMHOST
-	// define('FEE_INSCRIPTION', '26'); // Product registration fee
-	// define('FEE_GRADUATION', '30'); // Product graduation fee
-	// define('LATAM_SCHOLARSHIP', '62'); // Beca
-	// define('DUAL_9NO', '15'); // Product Initial 9 Grade (lower)
-	// define('DUAL_9NO_VARIABLE', '18'); // Product variable (lower)
-	// define('DUAL_10MO', '16');// Product Initial 10mo Grade (middle)
-	// define('DUAL_10MO_VARIABLE', '21'); // Product variable  (middle)
-	// define('DUAL_DEFAULT', '17');    // Product initial 11 and bacheloder (upper)
-	// define('DUAL_DEFAULT_VARIABLE','24'); // Product variable (upper)
+	define('FEE_INSCRIPTION', '26'); // Product registration fee
+	define('FEE_GRADUATION', '30'); // Product graduation fee
+	define('LATAM_SCHOLARSHIP', '62'); // Beca
+	define('DUAL_9NO', '15'); // Product Initial 9 Grade (lower)
+	define('DUAL_9NO_VARIABLE', '18'); // Product variable (lower)
+	define('DUAL_10MO', '16');// Product Initial 10mo Grade (middle)
+	define('DUAL_10MO_VARIABLE', '21'); // Product variable  (middle)
+	define('DUAL_DEFAULT', '17');    // Product initial 11 and bacheloder (upper)
+	define('DUAL_DEFAULT_VARIABLE','24'); // Product variable (upper)
 
 	// GLOBAL
 	define('ROLES_OF_STAFF', array('owner', 'administrador', 'admision', 'admission', 'alliance', 'administration', 'allied', 'webinar-aliance'));
 	define('PROGRAM_ID', 'HSD-01'); // AES program for endpoint for laravel create user and move documents
 	define('TYPE_PROGRAM', '0'); // AES type of program for endpoint for laravel create user and move documents
-	define('URL_LARAVEL_PPADMIN', 'https://admin.americanelite.school/'); 
+	define('URL_LARAVEL_PPADMIN', 'https://ppadmin.american-elite.us/'); 
 	define('ROLE_ID_STUDENT_MOODLE', 5); // Role ID moodle student
 	define('VERSIONS_JS', '2.0.59'); // Versions JS
