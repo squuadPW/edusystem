c0727182e7768ce28fe4baa10c4098fa46f87938
Jose Mora <jmoraw7@gmail.com>
2025-06-23 15:22:30 -0400

    fixes

--- admin/functions.php
+++ admin/functions.php
@@ -161,7 +161,7 @@ function aes_scripts_admin()
         wp_enqueue_script('teacher', plugins_url('edusystem') . '/admin/assets/js/teacher.js', array('jquery'), $version, true);
     }
 
-    if (isset($_GET['page']) && !empty($_GET['page']) && ($_GET['page'] == 'report-sales' || $_GET['page'] == 'add_admin_form_report_content') || $_GET['page'] == 'report-accounts-receivables' || $_GET['page'] == 'report-students' || $_GET['page'] == 'report-sales-product') {
+    if (isset($_GET['page']) && !empty($_GET['page']) && ($_GET['page'] == 'report-summary' || $_GET['page'] == 'add_admin_form_report_content') || $_GET['page'] == 'report-accounts-receivables' || $_GET['page'] == 'report-students' || $_GET['page'] == 'report-sales-product') {
         wp_enqueue_script('report', plugins_url('edusystem') . '/admin/assets/js/report.js', array('jquery'), $version, true);
         wp_enqueue_script('chart-js', 'https://cdn.jsdelivr.net/npm/chart.js');
         wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js');
@@ -457,7 +457,7 @@ function add_custom_admin_page()
             'dashicons-list-view',
             8
         );
-        add_submenu_page('add_admin_form_report_content', __('Summary', 'edusystem'), __('Summary', 'edusystem'), 'manager_sales_aes', 'report-sales', 'show_report_sales', 10);
+        add_submenu_page('add_admin_form_report_content', __('Summary', 'edusystem'), __('Summary', 'edusystem'), 'manager_sales_aes', 'report-summary', 'show_report_sales', 10);
         add_submenu_page('add_admin_form_report_content', __('Accounts receivable', 'edusystem'), __('Accounts receivable', 'edusystem'), 'manager_accounts_receivables_aes', 'report-accounts-receivables', 'show_report_accounts_receivables', 10);
         add_submenu_page('add_admin_form_report_content', __('Sales by product', 'edusystem'), __('Sales by product', 'edusystem'), 'manager_report_sales_product', 'report-sales-product', 'show_report_sales_product', 10);
         add_submenu_page('add_admin_form_report_content', __('Students', 'edusystem'), __('Students', 'edusystem'), 'manager_report_students_aes', 'report-students', 'show_report_current_students', 10);
