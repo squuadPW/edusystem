2f433705563094379f82a654bc2895d37f794c10
Jose Mora <jmoraw7@gmail.com>
2025-06-30 12:30:57 -0400

    versions

--- settings.php
+++ settings.php
@@ -27,4 +27,4 @@
 	define('TYPE_PROGRAM', '0'); // AES type of program for endpoint for laravel create user and move documents
 	define('URL_LARAVEL_PPADMIN', 'https://ppadmin.american-elite.us/'); 
 	define('ROLE_ID_STUDENT_MOODLE', 5); // Role ID moodle student
-	define('VERSIONS_JS', '2.0.69'); // Versions JS
+	define('VERSIONS_JS', '2.0.70'); // Versions JS
