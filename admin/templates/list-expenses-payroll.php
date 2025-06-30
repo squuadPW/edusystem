c43ad93b4cce0bbdcf36d65b946a86921ef00b98
Jose Mora <jmoraw7@gmail.com>
2025-06-30 11:18:49 -0400

    fixes

--- admin/templates/list-expenses-payroll.php
+++ admin/templates/list-expenses-payroll.php
@@ -11,6 +11,11 @@ include(plugin_dir_path(__FILE__) . 'topbar-payments.php');
 				<h1 class="wp-heading-line"><?= __('Expenses and payroll', 'edusystem'); ?></h1>
 			<?php endif; ?>
 		</div>
+
+		<?php 
+        	include(plugin_dir_path(__FILE__).'cookie-message.php');
+    	?>
+
 		<div style="display:flex;width:100%;justify-content:end;margin-bottom:10px;">
 			<a href="<?= admin_url('admin.php?page=add_admin_form_payments_content&section_tab=add_expenses_payroll'); ?>"
 				class="button button-outline-primary"><?= __('Add expense', 'edusystem'); ?></a>
