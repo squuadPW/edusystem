2f8d0fa843f0ec73740ae02b3562df61ebdb1fcb
Jose Mora <jmoraw7@gmail.com>
2025-07-03 17:52:24 -0400

    version

--- edusystem.php
+++ edusystem.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: EduSystem
 Description: Transform your WordPress into a complete, professional and scalable educational ecosystem.
-Version: 3.4.18
+Version: 3.4.19
 Author: EduSof
 Author URI: https://edusof.com/
 License:      GPL2
