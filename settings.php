73f7eb7034cd01f748f03e01e1577ee78c014aa2
Jose Mora <jmoraw7@gmail.com>
2025-07-11 09:45:04 -0400

    fixes

--- settings.php
+++ settings.php
@@ -3,35 +3,14 @@
 	// FGU AWS
 	// define('FEE_INSCRIPTION', '114'); // Product registration fee
 	// define('FEE_GRADUATION', '118'); // Product graduation fee
-	// define('LATAM_SCHOLARSHIP', '62'); // Beca
-	// define('DUAL_9NO', '103'); // Product Initial 9 Grade (lower)
-	// define('DUAL_9NO_VARIABLE', '106'); // Product variable (lower)
-	// define('DUAL_10MO', '104');// Product Initial 10mo Grade (middle)
-	// define('DUAL_10MO_VARIABLE', '109'); // Product variable  (middle)
-	// define('DUAL_DEFAULT', '105');    // Product initial 11 and bacheloder (upper)
-	// define('DUAL_DEFAULT_VARIABLE','112'); // Product variable (upper)
 
 	//AWS
 	// define('FEE_INSCRIPTION', '63'); // Product registration fee
 	// define('FEE_GRADUATION', '1591'); // Product graduation fee
-	// define('LATAM_SCHOLARSHIP', '62'); // Beca
-	// define('DUAL_9NO', '51'); // Product Initial 9 Grade (lower)
-	// define('DUAL_9NO_VARIABLE', '54'); // Product variable (lower)
-	// define('DUAL_10MO', '52');// Product Initial 10mo Grade (middle)
-	// define('DUAL_10MO_VARIABLE', '57'); // Product variable  (middle)
-	// define('DUAL_DEFAULT', '53');    // Product initial 11 and bacheloder (upper)
-	// define('DUAL_DEFAULT_VARIABLE','60'); // Product variable (upper)
 
 	// DREAMHOST
 	define('FEE_INSCRIPTION', '26'); // Product registration fee
 	define('FEE_GRADUATION', '30'); // Product graduation fee
-	define('LATAM_SCHOLARSHIP', '62'); // Beca
-	define('DUAL_9NO', '15'); // Product Initial 9 Grade (lower)15
-	define('DUAL_9NO_VARIABLE', '18'); // Product variable (lower)
-	define('DUAL_10MO', '16');// Product Initial 10mo Grade (middle)
-	define('DUAL_10MO_VARIABLE', '21'); // Product variable  (middle)
-	define('DUAL_DEFAULT', '669');    // Product initial 11 and bacheloder (upper)17
-	define('DUAL_DEFAULT_VARIABLE','24'); // Product variable (upper)
 
 	// GLOBAL
 	define('ROLES_OF_STAFF', array('owner', 'administrador', 'admision', 'admission', 'alliance', 'administration', 'allied', 'webinar-aliance'));
@@ -40,4 +19,6 @@
 	define('URL_LARAVEL_PPADMIN', 'https://ppadmin.american-elite.us/'); 
 	define('ROLE_ID_STUDENT_MOODLE', 5); // Role ID moodle student
 	define('VERSIONS_JS', '2.0.77'); // Versions JS
-	define('MODE', 'UNI'); // Versions JS
+	define('MODE', 'SCHOOL'); // Versions JS
+
+?>
