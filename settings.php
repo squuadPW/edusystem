ff0dd3a7bc98306642d4ec1067ada152e847dca0
Jose Mora <jmoraw7@gmail.com>
2025-07-22 17:43:27 -0400

    fixes

--- settings.php
+++ settings.php
@@ -9,20 +9,20 @@
 	// define('FEE_GRADUATION', '90'); // Product graduation fee
 
 	// portal.americanelite.school
-	// define('FEE_INSCRIPTION', '63'); // Product registration fee
-	// define('FEE_GRADUATION', '1591'); // Product graduation fee
+	define('FEE_INSCRIPTION', '63'); // Product registration fee
+	define('FEE_GRADUATION', '1591'); // Product graduation fee
 
 	// americanelite.dreamhosters.com & dev.edusof.com 
-	define('FEE_INSCRIPTION', '26'); // Product registration fee
-	define('FEE_GRADUATION', '30'); // Product graduation fee
+	// define('FEE_INSCRIPTION', '26'); // Product registration fee
+	// define('FEE_GRADUATION', '30'); // Product graduation fee
 
 	// GLOBAL
 	define('ROLES_OF_STAFF', array('owner', 'administrador', 'admision', 'admission', 'alliance', 'administration', 'allied', 'webinar-aliance', 'manager'));
 	define('PROGRAM_ID', 'HSD-01'); // AES program for endpoint for laravel create user and move documents
 	define('TYPE_PROGRAM', '0'); // AES type of program for endpoint for laravel create user and move documents
-	define('URL_LARAVEL_PPADMIN', 'https://ppadmin.americanelite.school/'); 
+	define('URL_LARAVEL_PPADMIN', 'https://admin.americanelite.school/'); 
 	define('ROLE_ID_STUDENT_MOODLE', 5); // Role ID moodle student
-	define('VERSIONS_JS', '2.0.92'); // Versions JS
+	define('VERSIONS_JS', '2.0.94'); // Versions JS
 	define('MODE', 'SCHOOL'); // Versions JS
 
 ?>
