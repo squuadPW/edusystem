a15ffea5ecffd464b4205392ae01ce46d19d77ea
Jose Mora <jmoraw7@gmail.com>
2025-07-24 09:51:23 -0400

    FIXES

--- settings.php
+++ settings.php
@@ -1,8 +1,8 @@
 <?php
 
 	// portal.floridaglobal.university
-	define('FEE_INSCRIPTION', '114'); // Product registration fee
-	define('FEE_GRADUATION', '118'); // Product graduation fee
+	// define('FEE_INSCRIPTION', '114'); // Product registration fee
+	// define('FEE_GRADUATION', '118'); // Product graduation fee
 
 	// portal.utbp.edu.mx
 	// define('FEE_INSCRIPTION', '86'); // Product registration fee
@@ -13,8 +13,8 @@
 	// define('FEE_GRADUATION', '1591'); // Product graduation fee
 
 	// americanelite.dreamhosters.com & dev.edusof.com 
-	// define('FEE_INSCRIPTION', '26'); // Product registration fee
-	// define('FEE_GRADUATION', '30'); // Product graduation fee
+	define('FEE_INSCRIPTION', '26'); // Product registration fee
+	define('FEE_GRADUATION', '30'); // Product graduation fee
 
 	// GLOBAL
 	define('ROLES_OF_STAFF', array('owner', 'administrador', 'admision', 'admission', 'alliance', 'administration', 'allied', 'webinar-aliance', 'manager'));
@@ -23,6 +23,6 @@
 	define('URL_LARAVEL_PPADMIN', 'https://ppadmin.americanelite.school/'); 
 	define('ROLE_ID_STUDENT_MOODLE', 5); // Role ID moodle student
 	define('VERSIONS_JS', '2.2.3'); // Versions JS
-	define('MODE', 'UNI'); // Versions JS
+	define('MODE', 'SCHOOL'); // Versions JS
 
 ?>
