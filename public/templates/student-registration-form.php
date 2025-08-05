b2ab90229190a83281fea83a2ec008c7fd682b54
Jose Mora <jmoraw7@gmail.com>
2025-08-05 15:08:02 -0400

    fixes

--- public/templates/student-registration-form.php
+++ public/templates/student-registration-form.php
@@ -203,12 +203,10 @@
             style="<?= $styles_shortcode ?>">
             <div class="subtitle text-align-center"><?= __('Degree details', 'edusystem'); ?></div>
         </div>
-        <div class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6" id="program_select">
+        <div class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6" id="program_select" style="display: <?= count($programs) > 1 ? 'block' : 'none' ?>;">
             <label for="program"><?= __('Program of your interest', 'edusystem'); ?><span class="required">*</span></label>
             <select name="program" id="program" autocomplete="off" required>
-                <?php if(count($programs) > 0) { ?>
-                    <option value="" selected="selected"><?= __('Select an option', 'edusystem'); ?></option>
-                <?php } ?>
+                <option value="" <?= count($programs) > 1 ? 'selected' : '' ?>><?= __('Select an option', 'edusystem'); ?></option>
                 <?php foreach ($programs as $program): ?>
                     <option value="<?= $program->id; ?>" <?= count($programs) == 1 ? 'selected' : '' ?>><?= $program->name; ?></option>
                 <?php endforeach; ?>
