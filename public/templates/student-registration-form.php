aed25f6f522fca4e355c127414da6cc4bea244cf
Jose Mora <jmoraw7@gmail.com>
2025-08-05 15:15:41 -0400

    fixes

--- public/templates/student-registration-form.php
+++ public/templates/student-registration-form.php
@@ -1,6 +1,4 @@
-<form method="POST"
-    action="<?php the_permalink(); ?>?action=save_student"
-    class="form-aes" autocomplete="off">
+<form method="POST" action="<?php the_permalink(); ?>?action=save_student" class="form-aes" autocomplete="off">
     <div id="loading"
         style="display: none; position: fixed; top: 0; left: 0; width: 100%; height: 100%; background-color: rgba(255, 255, 255, 0.8); text-align: center; font-weight: 600; font-style: italic; z-index: 1000;">
         <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);">
@@ -12,23 +10,26 @@
     <input type="hidden" name="crm_url" id="x-api-url" value="<?= get_option('crm_url') ?? ''; ?>">
     <input type="hidden" name="crm_id" id="x-api-id" value="<?= $_GET['crm_id'] ?? ''; ?>">
     <input type="hidden" name="crm_api" id="x-api" value="contacts">
-    <input type="hidden" name="squuad_stripe_selected_client_id" id="squuad_stripe_selected_client_id" value="<?= $connected_account ?? '' ?>">
+    <input type="hidden" name="squuad_stripe_selected_client_id" id="squuad_stripe_selected_client_id"
+        value="<?= $connected_account ?? '' ?>">
     <input type="hidden" name="coupon_code" id="coupon_code" value="<?= $coupon_code ?? '' ?>">
     <input type="hidden" name="flywire_portal_code" id="flywire_portal_code" value="<?= $flywire_portal_code ?? '' ?>">
     <input type="hidden" name="manager_user_id" id="manager_user_id" value="<?= $manager_user_id ?? '' ?>">
     <input type="hidden" name="zelle_account" id="zelle_account" value="<?= $zelle_account ?? '' ?>">
-    <input type="hidden" name="hidden_payment_methods" id="hidden_payment_methods" value="<?= $hidden_payment_methods ?? '' ?>">
+    <input type="hidden" name="hidden_payment_methods" id="hidden_payment_methods"
+        value="<?= $hidden_payment_methods ?? '' ?>">
     <input type="hidden" name="register_psp" id="register_psp" value="<?= $register_psp ?? '' ?>">
-    <input type="hidden" name="bank_transfer_account" id="bank_transfer_account" value="<?= $bank_transfer_account ?? '' ?>">
-    <input type="hidden" name="fixed_fee_inscription" id="fixed_fee_inscription" value="<?= $fixed_fee_inscription ?? '' ?>">
+    <input type="hidden" name="bank_transfer_account" id="bank_transfer_account"
+        value="<?= $bank_transfer_account ?? '' ?>">
+    <input type="hidden" name="fixed_fee_inscription" id="fixed_fee_inscription"
+        value="<?= $fixed_fee_inscription ?? '' ?>">
     <input type="hidden" name="max_age" id="max_age" value="<?= $max_age ?>">
     <input type="hidden" name="limit_age" id="limit_age" value="<?= $limit_age ?>">
     <input type="hidden" id="product_id_input" name="product_id">
 
     <!-- DATOS DEL ESTUDIANTE -->
     <div class="grid grid-cols-12 gap-4">
-        <div class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6"
-            style="<?= $styles_shortcode ?>">
+        <div class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6" style="<?= $styles_shortcode ?>">
             <div class="subtitle text-align-center"><?= __('Student details', 'edusystem'); ?></div>
         </div>
         <!-- <div class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6">
@@ -36,7 +37,8 @@
                 <select id="year-select" class="year-select"></select>
             </div> -->
         <div class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6">
-            <label for="birth_date_student"><?= __('Date of birth', 'edusystem'); ?><span class="required">*</span></label>
+            <label for="birth_date_student"><?= __('Date of birth', 'edusystem'); ?><span
+                    class="required">*</span></label>
             <input class="formdata flatpickr" autocomplete="off" type="date" id="birth_date_student"
                 name="birth_date_student" required>
             <input class="formdata" autocomplete="off" type="hidden" id="dont_allow_adult" name="dont_allow_adult"
@@ -62,18 +64,22 @@
         </div>
         <div class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6">
             <label for="name_student"><?= __('Name', 'edusystem'); ?><span class="required">*</span></label>
-            <input class="formdata capitalize" type="text" name="name_student" autocomplete="off" id="name_student" required>
+            <input class="formdata capitalize" type="text" name="name_student" autocomplete="off" id="name_student"
+                required>
         </div>
         <div class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6">
-            <label for="middle_name_student"><?= __('Second name', 'edusystem'); ?><span class="required">*</span></label>
+            <label for="middle_name_student"><?= __('Second name', 'edusystem'); ?><span
+                    class="required">*</span></label>
             <input class="formdata capitalize" type="text" name="middle_name_student" autocomplete="off" required>
         </div>
         <div class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6">
             <label for="lastname_student"><?= __('Last name', 'edusystem'); ?><span class="required">*</span></label>
-            <input class="formdata capitalize" type="text" name="lastname_student" autocomplete="off" id="lastname_student" required>
+            <input class="formdata capitalize" type="text" name="lastname_student" autocomplete="off"
+                id="lastname_student" required>
         </div>
         <div class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6">
-            <label for="middle_last_name_student"><?= __('Second last name', 'edusystem'); ?><span class="required">*</span></label>
+            <label for="middle_last_name_student"><?= __('Second last name', 'edusystem'); ?><span
+                    class="required">*</span></label>
             <input class="formdata capitalize" type="text" name="middle_last_name_student" autocomplete="off" required>
         </div>
         <div class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6">
@@ -82,7 +88,8 @@
         </div>
         <div class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6" id="student-email-detail">
             <div id="student-email">
-                <label for="email_student"><?= __('Email address', 'edusystem'); ?><span class="required">*</span></label>
+                <label for="email_student"><?= __('Email address', 'edusystem'); ?><span
+                        class="required">*</span></label>
                 <input class="formdata" type="email" name="email_student" autocomplete="off"
                     oninput="sendAjaxStudentEmailDocument()" required>
                 <span id="existstudentemail"
@@ -118,14 +125,16 @@
             <div class="subtitle text-align-center"><?= __('Parent details', 'edusystem'); ?></div>
         </div>
         <div id="parent_birth_date_field" class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6">
-            <label for="birth_date_parent"><?= __('Date of birth', 'edusystem'); ?><span class="required">*</span></label>
+            <label for="birth_date_parent"><?= __('Date of birth', 'edusystem'); ?><span
+                    class="required">*</span></label>
             <input class="formdata flatpickr" autocomplete="off" type="date" id="birth_date_parent"
                 name="birth_date_parent" required>
         </div>
 
         <div id="parent_document_type_field" class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6">
             <label for="document_type"><?= __('Type document', 'edusystem'); ?><span class="required">*</span></label>
-            <select id="parent_document_type" name="parent_document_type" autocomplete="off" oninput="validateIDs(false)" required>
+            <select id="parent_document_type" name="parent_document_type" autocomplete="off"
+                oninput="validateIDs(false)" required>
                 <option value="" selected="selected"><?= __('Select an option', 'edusystem'); ?></option>
                 <option value="passport"><?= __('Passport', 'edusystem'); ?></option>
                 <option value="identification_document"><?= __('Identification Document', 'edusystem'); ?></option>
@@ -134,9 +143,10 @@
         </div>
 
         <div id="parent_id_document_field" class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6">
-            <label for="id_document_parent"><?= __('ID document', 'edusystem'); ?><span class="required">*</span></label>
-            <input class="formdata capitalize" autocomplete="off" type="text" id="id_document_parent" name="id_document_parent"
-                oninput="validateIDs(false)" required>
+            <label for="id_document_parent"><?= __('ID document', 'edusystem'); ?><span
+                    class="required">*</span></label>
+            <input class="formdata capitalize" autocomplete="off" type="text" id="id_document_parent"
+                name="id_document_parent" oninput="validateIDs(false)" required>
             <span class="sameids"
                 style="font-style: italic; color: red; font-size: 12px; display: none"><?= __('The representative cannot share the same ID as the student', 'edusystem'); ?></span>
         </div>
@@ -178,9 +188,9 @@
         </div>
 
         <!-- DATOS DE ACCESO -->
-        <div class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6 mt-10"
-            style="<?= $styles_shortcode ?>">
-            <div class="subtitle text-align-center" id="access_data"><?= __('Platform access data of parent', 'edusystem'); ?>
+        <div class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6 mt-10" style="<?= $styles_shortcode ?>">
+            <div class="subtitle text-align-center" id="access_data">
+                <?= __('Platform access data of parent', 'edusystem'); ?>
             </div>
         </div>
         <div class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6" id="student-email-access"></div>
@@ -199,27 +209,32 @@
         </div>
 
         <!-- DATOS DEL GRADO -->
-        <div class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6 mt-10"
-            style="<?= $styles_shortcode ?>">
+        <div class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6 mt-10" style="<?= $styles_shortcode ?>">
             <div class="subtitle text-align-center"><?= __('Degree details', 'edusystem'); ?></div>
         </div>
-        <div class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6" id="program_select" style="display: <?= count($programs) > 1 ? 'block' : 'none' ?>;">
-            <label for="program"><?= __('Program of your interest', 'edusystem'); ?><span class="required">*</span></label>
+        <div class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6" id="program_select"
+            style="display: <?= count($programs) > 1 ? 'block' : 'none' ?>;">
+            <label for="program"><?= __('Program of your interest', 'edusystem'); ?><span
+                    class="required">*</span></label>
             <select name="program" id="program" autocomplete="off" required>
-                <option value="" <?= count($programs) > 1 ? 'selected' : '' ?>><?= __('Select an option', 'edusystem'); ?></option>
+                <option value="" <?= count($programs) > 1 ? 'selected' : '' ?>>
+                    <?= __('Select an option', 'edusystem'); ?></option>
                 <?php foreach ($programs as $program): ?>
-                    <option value="<?= $program->id; ?>" <?= count($programs) == 1 ? 'selected' : '' ?>><?= $program->name; ?></option>
+                    <option value="<?= $program->id; ?>" <?= count($programs) == 1 ? 'selected' : '' ?>><?= $program->name; ?>
+                    </option>
                 <?php endforeach; ?>
             </select>
         </div>
         <div class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6" id="careers_select" style="display: none">
-            <label for="career"><?= __('Career of your interest', 'edusystem'); ?><span class="required">*</span></label>
+            <label for="career"><?= __('Career of your interest', 'edusystem'); ?><span
+                    class="required">*</span></label>
             <select name="career" id="career" autocomplete="off" required>
                 <option value="" selected="selected"><?= __('Select an option', 'edusystem'); ?></option>
             </select>
         </div>
         <div class="col-start-1 sm:col-start-4 col-span-12 sm:col-span-6" id="mentions_select" style="display: none;">
-            <label for="mention"><?= __('Mention of your interest', 'edusystem'); ?><span class="required">*</span></label>
+            <label for="mention"><?= __('Mention of your interest', 'edusystem'); ?><span
+                    class="required">*</span></label>
             <select name="mention" id="mention" autocomplete="off" required>
                 <option value="" selected="selected"><?= __('Select an option', 'edusystem'); ?></option>
             </select>
@@ -228,7 +243,8 @@
             <label for="name_institute"><?= __('Name of School or Institution with Agreement', 'edusystem'); ?><span
                     id="institute_id_required" class="required">*</span></label>
             <select name="institute_id" autocomplete="off" id="institute_id" required>
-                <option value="" selected="selected" data-others="1"><?= __('Select an option', 'edusystem'); ?></option>
+                <option value="" selected="selected" data-others="1"><?= __('Select an option', 'edusystem'); ?>
+                </option>
                 <?php foreach ($institutes as $institute): ?>
                     <option value="<?= $institute->id; ?>" data-others="0" data-country="<?= $institute->country; ?>">
                         <?= $institute->name; ?>
