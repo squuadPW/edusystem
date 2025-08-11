f15f208fa46a7cd71ef5f62a56655c54a6becf99
Jose Mora <jmoraw7@gmail.com>
2025-08-11 14:57:58 -0400

    fixes

--- public/templates/pending-documents.php
+++ public/templates/pending-documents.php
@@ -11,7 +11,7 @@
                     <div class="document-container">
                         <div class="document-header">
                             <h4>
-                                <?= __('Pending Documents for ' . $student->name . ' ' . $student->last_name, 'edusystem'); ?>
+                                <?= __('Pending Documents for', 'edusystem') . ' ' .  $student->name . ' ' . $student->last_name; ?>
                             </h4>
                         </div>
                         
@@ -30,13 +30,13 @@
                                             <?php $name = get_name_document($document->document_id); ?>
                                             • <?= $name; ?>
                                             <?php if ($document->max_date_upload): ?>
-                                                <span class="deadline">- DEADLINE: <?= date('m/d/Y', strtotime($document->max_date_upload)) ?></span>
+                                                <span class="deadline">- <?= __('DEADLINE', 'edusystem') ?>: <?= date('m/d/Y', strtotime($document->max_date_upload)) ?></span>
                                             <?php endif; ?>
                                         </a>
                                     </span>
                                     
                                     <span class="document-status <?= $status_class ?>" style="<?= $is_required ?>">
-                                        <?= $status === 'No sent' ? 'Pending' : $status ?>
+                                        <?= $status === __('No sent', 'edusystem') ? __('Pending', 'edusystem') : __($status, 'edusystem') ?>
                                     </span>
                                 </div>
                             <?php endif; ?>
@@ -44,7 +44,7 @@
                         
                         <div class="upload-link">
                             <a href="<?= get_permalink(get_option('woocommerce_myaccount_page_id')) . '/student-documents'; ?>">
-                                Upload documents here
+                                <? __('Upload documents here', 'edusystem'); ?>
                             </a>
                         </div>
                     </div>
