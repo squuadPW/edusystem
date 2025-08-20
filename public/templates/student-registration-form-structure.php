2ee8f47670b822cf8d011ee1fbee41084bc1bacf
Jose Mora <jmoraw7@gmail.com>
2025-08-20 10:27:28 -0400

    fixes

--- public/templates/student-registration-form-structure.php
+++ public/templates/student-registration-form-structure.php
@@ -1,54 +1,47 @@
 <?php
+// Lógica de PHP al inicio del archivo
 if (is_user_logged_in()) {
-    ?>
-    <div class="title">
-        <?= $title ?>
-    </div>
-    <div class="title" style="font-size: 24px;">
-        <?= __('Applicant', 'edusystem'); ?>
-    </div>
-
-    <?php
-
     global $wpdb;
     $current_user = wp_get_current_user();
     $email = $current_user->user_email;
     $table_students = $wpdb->prefix . 'students';
+    $is_student = $wpdb->get_row($wpdb->prepare("SELECT * FROM $table_students WHERE email = %s", $email));
+}
 
-    $result = $wpdb->get_row($wpdb->prepare("SELECT * FROM $table_students WHERE email = %s", $email));
-    if (isset($result)) {
-        include(plugin_dir_path(__FILE__) . 'form-register-others.php');
-    } else {
-        ?>
+// Prepara la cadena de texto traducible para el JavaScript
+$tooltip_text = __('Please select the grade you are currently studying', 'edusystem');
+?>
+
+<?php if (!empty($title)) { ?>
+    <div class="title">
+        <?= $title ?>
+    </div>
+<?php } ?>
+<div class="title" style="font-size: 24px;">
+    <?= __('Applicant', 'edusystem'); ?>
+</div>
+<input type="hidden" id="grade_tooltip_content" value="<?= esc_attr($tooltip_text); ?>">
+
+<?php if (is_user_logged_in()) { ?>
+    <?php if (isset($is_student)) { ?>
+        <?php include(plugin_dir_path(__FILE__) . 'form-register-others.php'); ?>
+    <?php } else { ?>
         <section class="segment">
             <div class="segment-button active" data-option="me"><?= __('Me', 'edusystem'); ?></div>
             <div class="segment-button" data-option="others"><?= __('Others', 'edusystem'); ?></div>
         </section>
-
-        <?php
-        include(plugin_dir_path(__FILE__) . 'form-register-me.php');
-        include(plugin_dir_path(__FILE__) . 'form-register-others-not-student.php');
-    }
-
-?>
+        <?php include(plugin_dir_path(__FILE__) . 'form-register-me.php'); ?>
+        <?php include(plugin_dir_path(__FILE__) . 'form-register-others-not-student.php'); ?>
+    <?php } ?>
 <?php } else { ?>
-    <div class="title">
-        <?= $title ?>
-    </div>
-    <div class="title" style="font-size: 24px;">
-        <?= __('Applicant', 'edusystem'); ?>
-    </div>
-    <?php
-    include(plugin_dir_path(__FILE__) . 'student-registration-form.php');
-?>
+    <?php include(plugin_dir_path(__FILE__) . 'student-registration-form.php'); ?>
 <?php } ?>
 
 <script src="https://unpkg.com/@popperjs/core@2"></script>
 <script src="https://unpkg.com/tippy.js@6"></script>
 <script>
-    // With the above scripts loaded, you can call `tippy()` with a CSS
-    // selector and a `content` prop:
+    const tooltipText = document.getElementById('grade_tooltip_content').value;
     tippy('#grade_tooltip', {
-        content: 'Please select the grade you are currently studying',
+        content: tooltipText,
     });
 </script>
