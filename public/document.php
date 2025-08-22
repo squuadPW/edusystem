0f20451505055053c62d8a01e6db91d97bb9312f
Jose Mora <jmoraw7@gmail.com>
2025-08-22 11:18:00 -0400

    fixes

--- public/document.php
+++ public/document.php
@@ -55,7 +55,7 @@ add_action('woocommerce_account_student-documents_endpoint', function() {
         $students = get_student(get_current_user_id());
     }
     
-    $arr_photos_student = ['PHOTO OF STUDENT CARD', 'STUDENT\'S PHOTO', 'FOTO DEL ESTUDIANTE'];
+    $arr_photos_student = ['PHOTO OF STUDENT CARD', 'STUDENT\'S PHOTO', 'FOTO DEL ESTUDIANTE', "PHOTO-ID OR PASSPORT"];
     include(plugin_dir_path(__FILE__).'templates/documents.php');
 });
 
@@ -777,6 +777,22 @@ function get_help_info_document($document_id){
         $text = __('Please provide DIGITAL COPY OF POSTGRADUATE DEGREE to validate your information and continue with the registration. The allowed file type is ', 'edusystem') . get_type_file_document_teacher($document_id);
     } else if ($document_id == 'CURRICULAR SUMMARY') {
         $text = __('Please provide your CURRICULAR SUMMARY to validate your information and continue with the registration process. The allowed file type is ', 'edusystem') . get_type_file_document_teacher($document_id);
+    } else if ($document_id == 'IDENTIFICACION GUBERNAMENTAL CON FOTO') {
+        $text = __('GOVERNMENT-ISSUED PHOTO ID. (IDENTITY DOCUMENT OR PASSPORT OR DRIVER\'S LICENSE OR CEDULA OR DNI OR DIN)', 'edusystem') . get_type_file_document($document_id);
+    } else if ($document_id == 'FOTO TIPO CARNET O PASAPORTE') {
+        $text = __('PHOTO-ID OR PASSPORT', 'edusystem') . get_type_file_document($document_id);
+    } else if ($document_id == 'ÚLTIMO DIPLOMA DE GRADO OBTENIDO') {
+        $text = __('LAST DEGREE DIPLOMA OBTAINED. (NATIONAL OR FOREIGN)', 'edusystem') . get_type_file_document($document_id);
+    } else if ($document_id == 'REPORTE DE CALIFICACIONES O NOTAS OFICIALES DE LOS CURSOS APROBADOS EN UNA INSTITUCIÓN DE EDUCACIÓN SUPERIOR TSU O LICENCIATURA') {
+        $text = __('REPORT OF GRADES OR OFFICIAL TRANSCRIPTS OF COURSES PASSED AT A HIGHER EDUCATION INSTITUTION (TSU OR BACHELOR\'S DEGREE).', 'edusystem') . get_type_file_document($document_id);
+    } else if ($document_id == 'TRANSCRIPCIONES OFICIALES GED ORIGINAL DE LA ESCUELA SECUNDARIA') {
+        $text = __('OFFICIAL GED HIGH SCHOOL ORIGINAL TRANSCRIPTS', 'edusystem') . get_type_file_document($document_id);
+    } else if ($document_id == 'TRADUCCIÓN O ESCUELA SECUNDARIA EQUIVALENTE O GED POR INSTITUCIÓN RECONOCIDA') {
+        $text = __('TRANSLATION OR EQUIVALENT HIGH SCHOOL OR GED BY RECOGNIZED INSTITUTION', 'edusystem') . get_type_file_document($document_id);
+    } else if ($document_id == 'CURRICULUM VITAE') {
+        $text = __('CURRICULUM VITAE (PH D. ONLY)', 'edusystem') . get_type_file_document($document_id);
+    } else if ($document_id == 'IDEA DE TESIS A DESARROLLAR') {
+        $text = __('THESIS IDEA TO DEVELOP (PH.D. ONLY)', 'edusystem') . get_type_file_document($document_id);
     } else if ($document_id == 'ACTA O PARTIDA DE NACIMIENTO') {
         $text = __('Official document that certifies the birth of a person. The allowed file type is ', 'edusystem') . get_type_file_document($document_id);
     } else if ($document_id == 'COPIA DE IDENTIFICACIÓN OFICIAL (DNI) O PASAPORTE') {
