150828143da1e50018841bd075ea50f2a8f96c07
Jose Mora <jmoraw7@gmail.com>
2025-12-04 11:47:19 -0400

    fixes

--- public/assets/js/previous-form.js
+++ public/assets/js/previous-form.js
@@ -1,12 +1,20 @@
-document.addEventListener('DOMContentLoaded',function(){
-
-    if(document.getElementById('continue-checkout')){
-      document.getElementById('continue-checkout').addEventListener('click', function(event) {
-        document.getElementById('continue-checkout').disabled = true;
-        document.getElementById('continue-checkout').innerText = 'Loading...';
+document.addEventListener("DOMContentLoaded", function () {
+  if (document.getElementById("continue-checkout")) {
+    document
+      .getElementById("continue-checkout")
+      .addEventListener("click", function (event) {
+        document.getElementById("continue-checkout").disabled = true;
+        document.getElementById("continue-checkout").innerText = "Loading...";
         const XHR = new XMLHttpRequest();
-        XHR.open("POST", `${ajax_object.ajax_url}?action=use_previous_form_aes`, true);
-        XHR.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        XHR.open(
+          "POST",
+          `${ajax_object.ajax_url}?action=use_previous_form_aes`,
+          true
+        );
+        XHR.setRequestHeader(
+          "Content-type",
+          "application/x-www-form-urlencoded"
+        );
         XHR.responseType = "text";
         let params = `action=use_previous_form_aes`;
         params += `&use=${1}`;
@@ -17,56 +25,88 @@ document.addEventListener('DOMContentLoaded',function(){
               // Redirigir si se proporciona una URL
               // let response = JSON.parse(XHR.response);
               // window.location.href = response.data.redirect;
-              document.querySelector("input[name=birth_date_student]").value = getCookie("birth_date");
-              document.querySelector("select[name=document_type]").value = getCookie("document_type");
-              document.querySelector("input[name=id_document]").value = getCookie("id_document");
-              document.querySelector("input[name=name_student]").value = getCookie("name_student");
-              document.querySelector("input[name=middle_name_student]").value = getCookie("middle_name_student");
-              document.querySelector("input[name=lastname_student]").value = getCookie("last_name_student");
-              document.querySelector("input[name=middle_last_name_student]").value = getCookie("middle_last_name_student");
-              document.querySelector("input[name=number_phone]").value = getCookie("phone_student");
-              document.querySelector("input[name=email_student]").value = getCookie("email_student");
-              document.querySelector("select[name=gender]").value = getCookie("gender");
-              document.querySelector("select[name=country]").value = getCookie("billing_country");
-              // document.querySelector("select[name=country]").dispatchEvent(new Event("change"));
 
-              document.querySelector("input[name=city]").value = getCookie("billing_city");
-              document.querySelector("select[name=etnia]").value = getCookie("ethnicity");
-              document.querySelector("input[name=birth_date_parent]").value = getCookie("birth_date_parent");
-              document.querySelector("select[name=parent_document_type]").value = getCookie("parent_document_type");
-              document.querySelector("input[name=id_document_parent]").value = getCookie("id_document_parent");
-              document.querySelector("input[name=agent_name]").value = getCookie("agent_name");
-              document.querySelector("input[name=agent_last_name]").value = getCookie("agent_last_name");
-              document.querySelector("input[name=number_partner]").value = getCookie("number_partner");
-              document.querySelector("select[name=gender_parent]").value = getCookie("gender_parent");
-              document.querySelector("input[name=email_partner]").value = getCookie("email_partner");
-              document.querySelector("input[name=password]").value = getCookie("password");
-              // document.querySelector("select[name=program]").value = getCookie("program_id_number");
-              // document.querySelector("select[name=program]").dispatchEvent(new Event("change"));
+              const emailStudent = getCookie("email_student");
+              let emailParent = getCookie("email_partner"); // Asumiendo que 'email_partner' es el email_parent
 
-              // document.querySelector("select[name=institute_id]").value = getCookie("institute_id");
-              // document.querySelector("select[name=institute_id]").dispatchEvent(new Event("change"));
+              // Validación: si email_student es igual a email_parent, pon null al email_parent
+              if (emailStudent === emailParent) {
+                emailParent = null; // Asigna null a la variable si son iguales
+              }
 
-              // document.querySelector("select[name=grade]").value = getCookie("initial_grade");
-              // document.querySelector("select[name=grade]").dispatchEvent(new Event("change"));
-
-              document.getElementById('modal-continue-checkout').style.display = 'none';
+              document.querySelector("input[name=birth_date_student]").value =
+                getCookie("birth_date");
+              document.querySelector("select[name=document_type]").value =
+                getCookie("document_type");
+              document.querySelector("input[name=id_document]").value =
+                getCookie("id_document");
+              document.querySelector("input[name=name_student]").value =
+                getCookie("name_student");
+              document.querySelector("input[name=middle_name_student]").value =
+                getCookie("middle_name_student");
+              document.querySelector("input[name=lastname_student]").value =
+                getCookie("last_name_student");
+              document.querySelector(
+                "input[name=middle_last_name_student]"
+              ).value = getCookie("middle_last_name_student");
+              document.querySelector("input[name=number_phone]").value =
+                getCookie("phone_student");
+              document.querySelector("input[name=email_student]").value =
+                emailStudent; // Usa la variable
+              document.querySelector("select[name=gender]").value =
+                getCookie("gender");
+              document.querySelector("select[name=country]").value =
+                getCookie("billing_country");
+              document.querySelector("input[name=city]").value =
+                getCookie("billing_city");
+              document.querySelector("select[name=etnia]").value =
+                getCookie("ethnicity");
+              document.querySelector("input[name=birth_date_parent]").value =
+                getCookie("birth_date_parent");
+              document.querySelector(
+                "select[name=parent_document_type]"
+              ).value = getCookie("parent_document_type");
+              document.querySelector("input[name=id_document_parent]").value =
+                getCookie("id_document_parent");
+              document.querySelector("input[name=agent_name]").value =
+                getCookie("agent_name");
+              document.querySelector("input[name=agent_last_name]").value =
+                getCookie("agent_last_name");
+              document.querySelector("input[name=number_partner]").value =
+                getCookie("number_partner");
+              document.querySelector("select[name=gender_parent]").value =
+                getCookie("gender_parent");
+              document.querySelector("input[name=email_partner]").value =
+                emailParent; // Usa la variable que podría ser null
+              document.querySelector("input[name=password]").value =
+                getCookie("password");
+              document.getElementById("modal-continue-checkout").style.display =
+                "none";
             }
           } else {
-            document.getElementById('send-continue').disabled = false;
-            document.getElementById('send-continue').innerText = 'continue';
+            document.getElementById("send-continue").disabled = false;
+            document.getElementById("send-continue").innerText = "continue";
           }
         };
       });
-    }
-  
-    if(document.getElementById('quit-checkout')){
-      document.getElementById('quit-checkout').addEventListener('click', function(event) {
-        document.getElementById('quit-checkout').disabled = true;
-        document.getElementById('quit-checkout').innerText = 'Loading...';
+  }
+
+  if (document.getElementById("quit-checkout")) {
+    document
+      .getElementById("quit-checkout")
+      .addEventListener("click", function (event) {
+        document.getElementById("quit-checkout").disabled = true;
+        document.getElementById("quit-checkout").innerText = "Loading...";
         const XHR = new XMLHttpRequest();
-        XHR.open("POST", `${ajax_object.ajax_url}?action=use_previous_form_aes`, true);
-        XHR.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        XHR.open(
+          "POST",
+          `${ajax_object.ajax_url}?action=use_previous_form_aes`,
+          true
+        );
+        XHR.setRequestHeader(
+          "Content-type",
+          "application/x-www-form-urlencoded"
+        );
         XHR.responseType = "text";
         let params = `action=use_previous_form_aes`;
         params += `&use=${0}`;
@@ -75,13 +115,12 @@ document.addEventListener('DOMContentLoaded',function(){
           if (XHR.status === 200) {
             location.reload();
           } else {
-            document.getElementById('send-continue').disabled = false;
-            document.getElementById('send-continue').innerText = 'continue';
+            document.getElementById("send-continue").disabled = false;
+            document.getElementById("send-continue").innerText = "continue";
           }
         };
       });
-    }
-
+  }
 });
 
 function getCookie(name) {
